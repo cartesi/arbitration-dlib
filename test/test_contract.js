a79cbc4d589a7bf5dea92fa51b1815c93b6f41e9
Augusto Teixeira <augusto@impa.br>
2017-11-07 15:51:34 -0200

    Add small changes to partition test

--- test/test_contract.js
+++ test/test_contract.js
@@ -1,14 +1,13 @@
 const fs = require('fs');
 const solc = require('solc');
-const mocha = require('mocha')
 const Web3 = require('web3');
+const TestRPC = require("ethereumjs-testrpc");
+const mocha = require('mocha')
 const coMocha = require('co-mocha')
 
 expect = require('chai').expect;
 
 coMocha(mocha)
-
-var TestRPC = require("ethereumjs-testrpc");
 
 aliceKey = '0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d'
 bobKey = '0x6cbed15c793ce57650b9877cf6fa156fbef513c4e6134f022a85b1ffdd59b2a1'
@@ -45,8 +44,7 @@ describe('Testing partition contract', function() {
             "secretKey": aliceKey },
           { "balance": 100000000000000000000,
             "secretKey": bobKey }
-      ],
-      //"debug": true
+      ]
     }
     web3 = new Web3(TestRPC.provider(testrpcParameters));
 
@@ -69,7 +67,7 @@ describe('Testing partition contract', function() {
     bobHistory = [];
 
     finalTime = 50000;
-    querySize = 7;
+    querySize = 3;
     roundDuration = 3600;
     lastAggreement = Math.floor((Math.random() * finalTime - 1) + 1);
 
@@ -100,6 +98,7 @@ describe('Testing partition contract', function() {
     }).send({ from: aliceAddr, gas: 1500000 })
       .on('receipt');
 
+    // create empty arrays for query and reply
     queryArray = [];
     replyArray = [];
     for (i = 0; i < querySize; i++) queryArray.push(0);
@@ -107,13 +106,13 @@ describe('Testing partition contract', function() {
 
     while (true) {
       var i;
-      // check if the state is waiting hashes
+      // check if the state is WaitingHashes
       currentState = yield partitionContract.methods
         .currentState().call({ from: bobAddr });
       expect(currentState).to.equal('1');
 
       // get the query array and prepare response
-      // solidity cannot return dynamic array from function
+      // (loop since solidity cannot return dynamic array from function)
       for (i = 0; i < querySize; i++) {
         queryArray[i] = yield partitionContract.methods
           .queryArray(i).call({ from: bobAddr });
@@ -144,7 +143,6 @@ describe('Testing partition contract', function() {
         .on('receipt', function(receipt) {
           expect(receipt.events.HashesPosted).not.to.be.undefined;
         });
-
       returnValues = response.events.HashesPosted.returnValues;
 
       // find first last time of query where there was aggreement
@@ -158,7 +156,7 @@ describe('Testing partition contract', function() {
         }
       }
 
-      // check if the state is waiting query
+      // check if the state is WaitingQuery
       currentState = yield partitionContract.methods
         .currentState().call({ from: bobAddr });
       expect(currentState).to.equal('0');
@@ -185,6 +183,10 @@ describe('Testing partition contract', function() {
           });
         returnValues = response.events.DivergenceFound.returnValues;
         expect(+returnValues.timeOfDivergence).to.equal(lastAggreement);
+        // check if the state is DivergenceFound
+        currentState = yield partitionContract.methods
+          .currentState().call({ from: bobAddr });
+        expect(currentState).to.equal('4');
         break;
       } else {
         // send query with last queried time of aggreement
@@ -202,7 +204,6 @@ describe('Testing partition contract', function() {
       .send({ from: aliceAddr, gas: 1500000 });
   });
 
-
   it('Claimer timeout', function*() {
     this.timeout(15000)
 
@@ -214,16 +215,12 @@ describe('Testing partition contract', function() {
     }).send({ from: aliceAddr, gas: 1500000 })
       .on('receipt');
 
-    queryArray = [];
-    replyArray = [];
-    for (i = 0; i < querySize; i++) queryArray.push(0);
-    for (i = 0; i < querySize; i++) replyArray.push("");
-
-    // check if the state is waiting hashes
+    // check if the state is WaitingHashes
     currentState = yield partitionContract.methods
       .currentState().call({ from: bobAddr });
     expect(currentState).to.equal('1');
 
+    // mimic a waiting period of 3500 seconds
     response = yield sendRPC({jsonrpc: "2.0", method: "evm_increaseTime",
                               params: [3500], id: 0});
 
@@ -235,6 +232,7 @@ describe('Testing partition contract', function() {
         expect(error.message).to.have.string('VM Exception');
       });
 
+    // mimic a waiting period of 200 seconds
     response = yield sendRPC({jsonrpc: "2.0", method: "evm_increaseTime",
                               params: [200], id: 0});
 
@@ -245,7 +243,7 @@ describe('Testing partition contract', function() {
     returnValues = response.events.ChallengeEnded.returnValues;
     expect(+returnValues.theState).to.equal(2);
 
-    // check if the state is waiting query
+    // check if the state is ChallengerWon
     currentState = yield partitionContract.methods
       .currentState().call({ from: bobAddr });
     expect(currentState).to.equal('2');
@@ -267,18 +265,19 @@ describe('Testing partition contract', function() {
     }).send({ from: aliceAddr, gas: 1500000 })
       .on('receipt');
 
+    // check if the state is WaitingHashes
+    currentState = yield partitionContract.methods
+      .currentState().call({ from: bobAddr });
+    expect(currentState).to.equal('1');
+
+    // create empty arrays for query and reply
     queryArray = [];
     replyArray = [];
     for (i = 0; i < querySize; i++) queryArray.push(0);
     for (i = 0; i < querySize; i++) replyArray.push("");
 
-    // check if the state is waiting hashes
-    currentState = yield partitionContract.methods
-      .currentState().call({ from: bobAddr });
-    expect(currentState).to.equal('1');
-
     // get the query array and prepare response
-    // solidity cannot return dynamic array from function
+    // (loop since solidity cannot return dynamic array from function)
     for (i = 0; i < querySize; i++) {
         queryArray[i] = yield partitionContract.methods
             .queryArray(i).call({ from: bobAddr });
@@ -292,9 +291,9 @@ describe('Testing partition contract', function() {
         .on('receipt', function(receipt) {
             expect(receipt.events.HashesPosted).not.to.be.undefined;
         });
-
     returnValues = response.events.HashesPosted.returnValues;
 
+    // mimic a waiting period of 3500 seconds
     response = yield sendRPC({jsonrpc: "2.0", method: "evm_increaseTime",
                               params: [3500], id: 0});
 
@@ -306,6 +305,7 @@ describe('Testing partition contract', function() {
         expect(error.message).to.have.string('VM Exception');
       });
 
+    // mimic a waiting period of 200 seconds
     response = yield sendRPC({jsonrpc: "2.0", method: "evm_increaseTime",
                               params: [200], id: 0});
 
@@ -316,7 +316,7 @@ describe('Testing partition contract', function() {
     returnValues = response.events.ChallengeEnded.returnValues;
     expect(+returnValues.theState).to.equal(3);
 
-    // check if the state is waiting query
+    // check if the state is ClaimerWon
     currentState = yield partitionContract.methods
       .currentState().call({ from: bobAddr });
     expect(currentState).to.equal('3');
