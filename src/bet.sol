3458b8d85c6d30698d94ba12caf98d7f6ac5cfcd
Augusto Teixeira <augusto@impa.br>
2017-11-11 12:58:38 -0200

    Add costo to challenge in bet

--- src/bet.sol
+++ src/bet.sol
@@ -1,3 +1,7 @@
+// This is not the best way to do a bet between two players.
+// For better bets, use something similar to RANDAO's algorithm.
+// This should however be safe and more modular for other tasks.
+
 /// @title Betting contract
 pragma solidity ^0.4.0;
 
@@ -8,12 +12,14 @@ contract bet is timeAware {
   address public challenger;
   address public claimer;
 
-  bytes32 initialHash;
-  bytes32 claimedFinalHash;
+  bytes32 initialHash; // consensusual between challenger and claimer
+  bytes32 claimedFinalHash; // could be non-consensual
   uint public finalTime;
 
-  uint public timeOfLastMove;
-  uint public roundDuration;
+  uint public timeOfLastMove; // last time someone made a move with deadline
+  uint public roundDuration; // time interval one has before expiration
+
+  uint public challengeCost; // price to start a challenge
 
   partition partitionContract;
 
@@ -29,8 +35,8 @@ contract bet is timeAware {
   event ChallengePosted(address thePartitionContract);
   event WinerFound(state finalState);
 
-  function bet(address theChallenger, address theClaimer,
-               uint theFinalTime, uint theRoundDuration) payable public {
+  function bet(address theChallenger, address theClaimer, uint theFinalTime,
+               uint theRoundDuration, uint theChallengeCost) payable public {
     require(theChallenger != theClaimer);
     challenger = theChallenger;
     claimer = theClaimer;
@@ -41,6 +47,7 @@ contract bet is timeAware {
     timeOfLastMove = getTime();
 
     initialHash = block.blockhash(block.number - 1);
+    challengeCost = theChallengeCost;
 
     currentState = state.WaitingClaim;
     BetStarted(challenger, claimer, initialHash,
@@ -48,7 +55,7 @@ contract bet is timeAware {
   }
 
   /// @notice Posts a claim (can only be called by claimer)
-  /// @param theClaimedFinalHash the hash that is inteded to be claimed as final
+  /// @param theClaimedFinalHash the hash that is claimed to be the final
   function postClaim(bytes32 theClaimedFinalHash) public {
     require(msg.sender == claimer);
     require(currentState == state.WaitingClaim);
@@ -59,9 +66,10 @@ contract bet is timeAware {
   }
 
   /// @notice Post a challenge to claimed final hash (only challenger can do)
-  function postChallenge() public {
+  function postChallenge() public payable {
     require(msg.sender == challenger);
     require(currentState == state.WaitingChallenge);
+    require(msg.value > challengeCost); // one needs to pay to post a challenge
     partitionContract = new partition(challenger, claimer,
                                       initialHash, claimedFinalHash,
                                       finalTime, 10, roundDuration);
@@ -69,29 +77,24 @@ contract bet is timeAware {
     ChallengePosted(partitionContract);
   }
 
-  /// @notice Challenger claims victory
+  /// @notice Claim victory (challenger only)
   function challengerClaimVictory() public {
     require(msg.sender == challenger);
-    // timeout to submit claim
+    bool won = false;
+    // claimer lost deadline to submit claim
     if (currentState == state.WaitingClaim
         && getTime() > timeOfLastMove + roundDuration) {
-      currentState = state.ChallengerWon;
-      WinerFound(currentState);
-      selfdestruct(challenger);
+      won = true;
     }
-    // even number on dice
+    // the claimed number is even (challenger's luck)
     if (currentState == state.WaitingChallenge
         && uint(claimedFinalHash) % 2 == 0) {
-      currentState = state.ChallengerWon;
-      WinerFound(currentState);
-      selfdestruct(challenger);
+      won = true;
     }
-    // challenger won the partition challenge
+    // challenger won the partition challenge (claimer misses deadline there)
     if (currentState == state.WaitingResolution
         && partitionContract.currentState() == partition.state.ChallengerWon) {
-      currentState = state.ChallengerWon;
-      WinerFound(currentState);
-      selfdestruct(challenger);
+      won = true;
     }
     // partition challenge ended in divergence and challenger won divergence
     if (currentState == state.WaitingResolution
@@ -101,29 +104,29 @@ contract bet is timeAware {
       bytes32 afterDivergence = partitionContract
         .timeHash(partitionContract.divergenceTime() + 1);
       if (keccak256(beforeDivergence) != afterDivergence) {
-        currentState = state.ChallengerWon;
-        WinerFound(currentState);
-        selfdestruct(challenger);
+        won = true;
       }
+    }
+    if (won) {
+      currentState = state.ChallengerWon;
+      WinerFound(currentState);
+      selfdestruct(challenger);
     }
   }
 
-  /// @notice Claimer claims victory
+  /// @notice Claim victory (claimer only)
   function claimerClaimVictory() public {
     require(msg.sender == claimer);
+    bool won = false;
     // timeout to submit challenge
     if (currentState == state.WaitingChallenge
         && getTime() > timeOfLastMove + roundDuration) {
-      currentState = state.ClaimerWon;
-      WinerFound(currentState);
-      selfdestruct(claimer);
+      won = true;
     }
     // claimer won the partition challenge
     if (currentState == state.WaitingResolution
         && partitionContract.currentState() == partition.state.ClaimerWon) {
-      currentState = state.ClaimerWon;
-      WinerFound(currentState);
-      selfdestruct(claimer);
+      won = true;
     }
     // partition challenge ended in divergence and claimer won divergence
     if (currentState == state.WaitingResolution
@@ -133,10 +136,13 @@ contract bet is timeAware {
       bytes32 afterDivergence = partitionContract
         .timeHash(partitionContract.divergenceTime() + 1);
       if (keccak256(beforeDivergence) == afterDivergence) {
-        currentState = state.ClaimerWon;
-        WinerFound(currentState);
-        selfdestruct(claimer);
+        won = true;
       }
+    }
+    if (won) {
+      currentState = state.ClaimerWon;
+      WinerFound(currentState);
+      selfdestruct(claimer);
     }
   }
 }
