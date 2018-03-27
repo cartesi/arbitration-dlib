df7325d971513bfdbeb2ce1b30f93a4d9f4f380b
Augusto Teixeira <augusto@impa.br>
2018-03-27 17:59:51 -0300

    Turning subleq to library

--- src/subleq.sol
+++ src/subleq.sol
@@ -1,50 +1,51 @@
 /// @title Subleq machine contract
 pragma solidity ^0.4.18;
 
-import "./mortal.sol";
-
 contract mmContract {
   function read(uint64 theAddress) public view returns (bytes8);
   function write(uint64 theAddress, bytes8 theValue) public;
   function finishWritePhase() public;
 }
 
-contract subleq is mortal {
-
-  mmContract private memoryManager;
-
-  // use storage because of solidity's problem with locals ("Stack too deep")
-  uint64 pcPosition;
-  uint64 icPosition;
-  uint64 ocPosition;
-  uint64 haltedState;
-  uint64 pc;
-  uint64 ic;
-  uint64 oc;
-  uint64 hs;
-  int64 memAddrA;
-  int64 memAddrB;
-  int64 memAddrC;
-  uint64 ramSize;
-  uint64 inputMaxSize;
-  uint64 outputMaxSize;
+library subleqLib {
 
   event StepGiven(uint8 exitCode);
 
-  function subleq(address memoryManagerAddress,
-                  uint64 theRamSize, uint64 theInputMaxSize,
-                  uint64 theOutputMaxSize) public
+  struct subleqCtx {
+    mmContract memoryManager;
+    address owner;
+
+    // use storage because of solidity's problem with locals ("Stack too deep")
+    uint64 pcPosition;
+    uint64 icPosition;
+    uint64 ocPosition;
+    uint64 haltedState;
+    uint64 pc;
+    uint64 ic;
+    uint64 oc;
+    uint64 hs;
+    int64 memAddrA;
+    int64 memAddrB;
+    int64 memAddrC;
+    uint64 ramSize;
+    uint64 inputMaxSize;
+    uint64 outputMaxSize;
+  }
+
+  function init(subleqCtx storage self, address memoryManagerAddress,
+                uint64 theRamSize, uint64 theInputMaxSize,
+                uint64 theOutputMaxSize) public
   {
-    require(ramSize < 0x0000ffffffffffff);
-    ramSize = theRamSize;
-    inputMaxSize = theInputMaxSize;
-    outputMaxSize = theOutputMaxSize;
-    memoryManager = mmContract(memoryManagerAddress);
+    require(theRamSize < 0x0000ffffffffffff);
+    self.ramSize = theRamSize;
+    self.inputMaxSize = theInputMaxSize;
+    self.outputMaxSize = theOutputMaxSize;
+    self.memoryManager = mmContract(memoryManagerAddress);
   }
 
   /// @notice Performs one step of the subleq machine on memory
   /// @return false indicates a halted machine or invalid instruction
-  function step() public returns (uint8)
+  function step(subleqCtx storage self) public returns (uint8)
   {
     // Architecture
     // +----------------+----------------+----------------+----------------+
@@ -65,88 +66,89 @@ contract subleq is mortal {
     // 11 -
     // 12 -
     // 13 -
-    require(msg.sender == owner);
+    require(msg.sender == self.owner);
 
-    pcPosition = 0x4000000000000000;
-    icPosition = 0x4000000000000008;
-    ocPosition = 0x4000000000000010;
-    haltedState = 0x4000000000000018;
-    pc = uint64(memoryManager.read(pcPosition));
-    ic = uint64(memoryManager.read(icPosition));
-    oc = uint64(memoryManager.read(ocPosition));
-    hs = uint64(memoryManager.read(haltedState));
-    memAddrA = int64(memoryManager.read(pc));
-    memAddrB = int64(memoryManager.read(pc + 8));
-    memAddrC = int64(memoryManager.read(pc + 16));
+    self.pcPosition = 0x4000000000000000;
+    self.icPosition = 0x4000000000000008;
+    self.ocPosition = 0x4000000000000010;
+    self.haltedState = 0x4000000000000018;
+    self.pc = uint64(self.memoryManager.read(self.pcPosition));
+    self.ic = uint64(self.memoryManager.read(self.icPosition));
+    self.oc = uint64(self.memoryManager.read(self.ocPosition));
+    self.hs = uint64(self.memoryManager.read(self.haltedState));
+    self.memAddrA = int64(self.memoryManager.read(self.pc));
+    self.memAddrB = int64(self.memoryManager.read(self.pc + 8));
+    self.memAddrC = int64(self.memoryManager.read(self.pc + 16));
 
     // if first or second operator are < -1, throw
-    if (hs != 0x0000000000000000) { StepGiven(1); return 1; }
-    if (memAddrA < -1) { StepGiven(2); return 2; }
-    if (memAddrB < -1) { StepGiven(3); return 3; }
-    if (memAddrA == -1 && memAddrB == -1) { StepGiven(4); return 4; }
-    if (memAddrA >= 0 && uint64(memAddrA) > ramSize)
-       { StepGiven(5); return 5; }
-    if (memAddrB >= 0 && uint64(memAddrB) > ramSize)
-       { StepGiven(6); return 6; }
+    if (self.hs != 0x0000000000000000) { emit StepGiven(1); return 1; }
+    if (self.memAddrA < -1) { emit StepGiven(2); return 2; }
+    if (self.memAddrB < -1) { emit StepGiven(3); return 3; }
+    if (self.memAddrA == -1 && self.memAddrB == -1)
+      { emit StepGiven(4); return 4; }
+    if (self.memAddrA >= 0 && uint64(self.memAddrA) > self.ramSize)
+       { emit StepGiven(5); return 5; }
+    if (self.memAddrB >= 0 && uint64(self.memAddrB) > self.ramSize)
+       { emit StepGiven(6); return 6; }
     // if first operator is -1, read from input
-    if (memAddrA == -1) {
+    if (self.memAddrA == -1) {
       // test if input is out of range
-      if (ic - 0x8000000000000000 > inputMaxSize) {
-        StepGiven(8); return 8;
+      if (self.ic - 0x8000000000000000 > self.inputMaxSize) {
+        emit StepGiven(8); return 8;
       }
       // read input at ic
-      bytes8 loaded = memoryManager.read(ic);
-      memoryManager.write(uint64(memAddrB) * 8, loaded);
+      bytes8 loaded = self.memoryManager.read(self.ic);
+      self.memoryManager.write(uint64(self.memAddrB) * 8, loaded);
       // increment ic
-      memoryManager.write(icPosition, bytes8(ic + 8));
+      self.memoryManager.write(self.icPosition, bytes8(self.ic + 8));
       // increment pc by three words
-      memoryManager.write(pcPosition, bytes8(pc + 24));
-      memoryManager.finishWritePhase();
-      StepGiven(0);
+      self.memoryManager.write(self.pcPosition, bytes8(self.pc + 24));
+      self.memoryManager.finishWritePhase();
+      emit StepGiven(0);
       return 0;
     }
     // if first operator is non-negative, load the memory address
-    bytes8 valueA = memoryManager.read(uint64(memAddrA) * 8);
+    bytes8 valueA = self.memoryManager.read(uint64(self.memAddrA) * 8);
     // if first operator is non-negative, but second operator is -1,
     // write to output
-    if (memAddrB == -1) {
+    if (self.memAddrB == -1) {
       // test if output is out of range
-      if (oc - 0xc000000000000000 > outputMaxSize) {
-        StepGiven(9); return 9;
+      if (self.oc - 0xc000000000000000 > self.outputMaxSize) {
+        emit StepGiven(9); return 9;
       }
       // write contents addressed by first operator into output
-      memoryManager.write(oc, valueA);
+      self.memoryManager.write(self.oc, valueA);
       // increment oc
-      memoryManager.write(ocPosition, bytes8(oc + 8));
+      self.memoryManager.write(self.ocPosition, bytes8(self.oc+ 8));
       // increment pc by three words
-      memoryManager.write(pcPosition, bytes8(pc + 24));
-      memoryManager.finishWritePhase();
+      self.memoryManager.write(self.pcPosition, bytes8(self.pc + 24));
+      self.memoryManager.finishWritePhase();
       // cancelling this rule of halting on negative write
       // if (int64(valueA) < 0) { memoryManager.write(haltedState, 1); }
-      StepGiven(0);
+      emit StepGiven(0);
       return 0;
     }
     // if valueB is non-negative, make the subleq operation
-    bytes8 valueB = memoryManager.read(uint64(memAddrB) * 8);
+    bytes8 valueB = self.memoryManager.read(uint64(self.memAddrB) * 8);
     bytes8 subtraction = bytes8(int64(valueB) - int64(valueA));
     // write subtraction to memory addressed by second operator
-    memoryManager.write(uint64(memAddrB) * 8, subtraction);
+    self.memoryManager.write(uint64(self.memAddrB) * 8, subtraction);
     if (int64(subtraction) <= 0) {
-      if (uint64(memAddrC) > ramSize) { StepGiven(7); return 7; }
-      if (memAddrC < 0) {
+      if (uint64(self.memAddrC) > self.ramSize) { emit StepGiven(7); return 7; }
+      if (self.memAddrC < 0) {
         // halt machine
-        memoryManager.write(haltedState, 1);
-        memoryManager.finishWritePhase();
-        StepGiven(0);
+        self.memoryManager.write(self.haltedState, 1);
+        self.memoryManager.finishWritePhase();
+        emit StepGiven(0);
         return 0;
       }
-      memoryManager.write(pcPosition, bytes8(memAddrC * 8));
-      memoryManager.finishWritePhase();
-      StepGiven(0);
+      self.memoryManager.write(self.pcPosition, bytes8(self.memAddrC * 8));
+      self.memoryManager.finishWritePhase();
+      emit StepGiven(0);
       return 0;
     }
-    memoryManager.write(pcPosition, bytes8(pc + 24));
-    StepGiven(0);
+    self.memoryManager.write(self.pcPosition, bytes8(self.pc + 24));
+    emit StepGiven(0);
     return 0;
   }
 }
