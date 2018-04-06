5e98bafb3f9f75d439f15dce35b4f3215f9c499c
Augusto Teixeira <augusto@cartesi.io>
2018-04-06 14:32:45 -0300

    Add depth to truffle and library

--- test/depth.js
+++ test/depth.js
@@ -1,38 +1,18 @@
-const fs = require('fs');
-const solc = require('solc');
-const Web3 = require('web3');
-const TestRPC = require("ethereumjs-testrpc");
-const mocha = require('mocha')
-const coMocha = require('co-mocha')
+const mm = require('../utils/mm.js');
 const BigNumber = require('bignumber.js');
+const Web3 = require('web3');
 
-const mm = require('../utils/mm.js')
+//var provider = new Web3.providers.HttpProvider("http://127.0.0.1:9545/");
 
-expect = require('chai').expect;
+var web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:9545'));
+//web3.setProvider(provider);
+var expect = require('chai').expect;
+var getEvent = require('../utils/tools.js').getEvent;
+var unwrap = require('../utils/tools.js').unwrap;
+var getError = require('../utils/tools.js').getError;
+var sendRPC = require('../utils/tools.js').sendRPC;
 
-coMocha(mocha)
-
-aliceKey = '0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d'
-bobKey = '0x6cbed15c793ce57650b9877cf6fa156fbef513c4e6134f022a85b1ffdd59b2a1'
-
-aliceAddr = '0x90f8bf6a479f320ead074411a4b0e7944ea8c9c1'
-bobAddr = '0xffcf8fdee72ac11b5c542428b35eef5769c409f0'
-
-// compile contract
-const contractSource = fs.readFileSync('src/depth.sol').toString();
-
-// using solc package for node
-const compiledContract = solc.compile(contractSource, 1);
-expect(compiledContract.errors, compiledContract.errors).to.be.undefined;
-const bytecode = compiledContract.contracts[':depth'].bytecode;
-const abi = JSON.parse(compiledContract.contracts[':depth'].interface);
-
-// using solc from the command line
-// const { exec } = require('child_process');
-// exec('/home/cortex/solidity/build/solc/solc -o /home/cortex/project/contracts --abi --bin /home/cortex/contracts/src/partition.sol', (err, stdout, stderr) => {
-// });
-// const bytecode = fs.readFileSync('src/partition.bin').toString();
-// const abi = JSON.parse(fs.readFileSync('src/partition.abi').toString());
+var DepthInterface = artifacts.require("./DepthInterface.sol");
 
 var aliceMM = new mm.MemoryManager();
 var bobMM = new mm.MemoryManager();
@@ -40,31 +20,8 @@ var zero = BigNumber('0');
 var small = BigNumber('1024');
 var large = BigNumber('18446744073709551360');
 
-
-describe('Testing depth contract', function() {
+contract('DepthInterface', function(accounts) {
   beforeEach(function() {
-    this.timeout(15000)
-    // testrpc
-    var testrpcParameters = {
-      "accounts":
-      [   { "balance": 100000000000000000000,
-            "secretKey": aliceKey },
-          { "balance": 100000000000000000000,
-            "secretKey": bobKey }
-      ]
-    }
-    web3 = new Web3(TestRPC.provider(testrpcParameters));
-
-    // promisify jsonRPC direct call
-    sendRPC = function(param){
-      return new Promise(function(resolve, reject){
-        web3.currentProvider.sendAsync(param, function(err, data){
-          if(err !== null) return reject(err);
-          resolve(data);
-        });
-      });
-    }
-
     values = {
       '0': '0x1111111111111111',
       '8': '0x1111111111111111',
@@ -86,39 +43,24 @@ describe('Testing depth contract', function() {
     bobMM.setValue('18446744073709551368', '0x1111111111111112');
 
     roundDuration = 3600;
-
-    // create contract object
-    depthContract = new web3.eth.Contract(abi);
   });
 
-  it.only('Find divergence', function*() {
-    this.timeout(15000)
-
+  it('Find divergence', async function() {
     // deploy contract and update object
-    depthContract = yield depthContract.deploy({
-      data: bytecode,
-      arguments: [aliceAddr, bobAddr, bobMM.merkel(), roundDuration]})
-      .send({ from: aliceAddr, gas: 1500000 })
-      .on('receipt');
-
-    // this line should leave after they fix this bug
-    // https://github.com/ethereum/web3.js/issues/1266
-    depthContract.setProvider(web3.currentProvider)
+    let depthInterface = await DepthInterface
+        .new(accounts[0], accounts[1], bobMM.merkel(), roundDuration,
+             { from: accounts[2], gas: 2000000 });
 
     let leftHash, rightHash;
 
     while (true) {
       // check if the state is WaitingHashes
-      currentState = yield depthContract.methods
-        .currentState().call({ from: bobAddr });
-      expect(currentState).to.equal('1');
+      currentState = await depthInterface.currentState.call();
+      expect(currentState.toNumber()).to.equal(1);
 
+      currentDepth = await depthInterface.currentDepth.call();
 
-      currentDepth = yield depthContract.methods
-        .currentDepth().call({ from: bobAddr });
-
-      currentAddress = yield depthContract.methods
-        .currentAddress().call({ from: bobAddr });
+      currentAddress = await depthInterface.currentAddress.call();
       currentSize = BigNumber(2).pow(63 - currentDepth);
       currentLeftAddress = BigNumber(currentAddress);
       currentRightAddress = currentLeftAddress.plus(currentSize);
@@ -131,43 +73,31 @@ describe('Testing depth contract', function() {
 
 
       // sending hashes from alice should fail
-      response = yield depthContract.methods
-        .replyQuery(leftHash, rightHash)
-        .send({ from: aliceAddr, gas: 1500000 })
-        .catch(function(error) {
-          expect(error.message).to.have.string('VM Exception');
-        });
-
+      expect(await getError(
+        depthInterface.replyQuery(leftHash, rightHash,
+                                  { from: accounts[0], gas: 1500000 }))
+            ).to.have.string('VM Exception');
 
       // alice claiming victory should fail
-      response = yield depthContract.methods
-        .claimVictoryByTime()
-        .send({ from: aliceAddr, gas: 1500000 })
-        .catch(function(error) {
-          expect(error.message).to.have.string('VM Exception');
-        });
-
-
+      expect(await getError(
+        depthInterface.claimVictoryByTime({ from: accounts[0], gas: 1500000 }))
+            ).to.have.string('VM Exception');
 
       // send hashes
-      response = yield depthContract.methods
-        .replyQuery(leftHash, rightHash)
-        .send({ from: bobAddr, gas: 1500000 })
-        .on('receipt', function(receipt) {
-          expect(receipt.events.HashesPosted).not.to.be.undefined;
-        });
-      returnValues = response.events.HashesPosted.returnValues;
+      response = await depthInterface
+        .replyQuery(leftHash, rightHash,
+                    { from: accounts[1], gas: 1500000 })
+      event = getEvent(response, 'HashesPosted');
+      expect(event).not.to.be.undefined;
 
       // get the hashes of children in alice memory to prepare query
       leftHash = aliceMM.subMerkel(aliceMM.memoryMap, currentLeftAddress,
-                                       60 - currentDepth);
+                                   60 - currentDepth);
       rightHash = aliceMM.subMerkel(aliceMM.memoryMap, currentRightAddress,
-                                     60 - currentDepth);
+                                    60 - currentDepth);
       // decide where to turn
-      claimerLeftHash = yield depthContract.methods
-        .claimerLeftChildHash().call({ from: aliceAddr });
-      claimerRightHash = yield depthContract.methods
-        .claimerRightChildHash().call({ from: aliceAddr });
+      claimerLeftHash = await depthInterface.claimerLeftChildHash.call();
+      claimerRightHash = await depthInterface.claimerRightChildHash.call();
       let continueToTheLeft = false;
       let differentHash;
       if (claimerLeftHash === leftHash) {
@@ -179,177 +109,160 @@ describe('Testing depth contract', function() {
       }
 
       // check if the state is WaitingQuery
-      currentState = yield depthContract.methods
-        .currentState().call({ from: bobAddr });
-      expect(currentState).to.equal('0');
+      currentState = await depthInterface.currentState.call();
+      expect(currentState.toNumber()).to.equal(0);
 
       // bob claiming victory should fail
-      response = yield depthContract.methods
-        .claimVictoryByTime()
-        .send({ from: bobAddr, gas: 1500000 })
-        .catch(function(error) {
-          expect(error.message).to.have.string('VM Exception');
-        });
+      expect(await getError(
+        depthInterface.claimVictoryByTime({ from: accounts[1], gas: 1500000 }))
+            ).to.have.string('VM Exception');
+
       // send query with last queried time of aggreement
-      response = yield depthContract.methods
-        .makeQuery(continueToTheLeft, differentHash)
-        .send({ from: aliceAddr, gas: 1500000 })
-        .on('receipt', function(receipt) {
-          expect(receipt.events.QueryPosted).not.to.be.undefined;
-        });
+      response = await depthInterface
+        .makeQuery(continueToTheLeft, differentHash,
+                   { from: accounts[0], gas: 1500000 })
+      expect(getEvent(response, 'QueryPosted')).not.to.be.undefined;
+
       // if the state is WaitingControversialPhrase, exit while loop
       // check if the state is WaitingQuery
-      currentState = yield depthContract.methods
-        .currentState().call({ from: bobAddr });
-      if (currentState === '4') break;
+      currentState = await depthInterface.currentState.call();
+      if (currentState.toNumber() === 4) break;
     }
-    currentAddress = yield depthContract.methods
-      .currentAddress().call({ from: bobAddr });
+    currentAddress = await depthInterface.currentAddress.call();
     currentAddress = BigNumber(currentAddress);
-
 
     let word1 = bobMM.getWord(currentAddress);
     let word2 = bobMM.getWord(currentAddress.plus(8));
     let word3 = bobMM.getWord(currentAddress.plus(16));
     let word4 = bobMM.getWord(currentAddress.plus(24));
     // send controversial hash
-    response = yield depthContract.methods
-      .postControversialPhrase(word1, word2, word3, word4)
-      .send({ from: bobAddr, gas: 1500000 })
-      .on('receipt', function(receipt) {
-        expect(receipt.events.ControversialPhrasePosted).not.to.be.undefined;
-      });
+    response = await depthInterface
+      .postControversialPhrase(word1, word2, word3, word4,
+                               { from: accounts[1], gas: 1500000 })
+    expect(getEvent(response, 'ControversialPhrasePosted')).not.to.be.undefined;
+
     // check if the state is WaitingQuery
-    currentState = yield depthContract.methods
-      .currentState().call({ from: bobAddr });
-    expect(currentState).to.equal('5');
-    divergingAddress = yield depthContract.methods
-      .currentAddress().call({ from: bobAddr });
-    expect(divergingAddress).to.equal(large.toString());
+    currentState = await depthInterface.currentState.call();
+    expect(currentState.toNumber()).to.equal(5);
+
+    divergingAddress = await depthInterface.currentAddress.call();
+    expect(divergingAddress.toString()).to.equal(large.toString());
+
     // kill contract
-    response = yield depthContract.methods.kill()
-      .send({ from: aliceAddr, gas: 1500000 });
+    response = await depthInterface.kill({ from: accounts[2], gas: 1500000 });
+
+    // check if contract was killed
+    [error, currentState] = await unwrap(depthInterface.currentState());
+    expect(error.message).to.have.string('not a contract address');;
   });
 
-  it('Claimer timeout', function*() {
-    this.timeout(15000)
-
+  it('Claimer timeout', async function() {
     // deploy contract and update object
-    depthContract = yield depthContract.deploy({
-      data: bytecode,
-      arguments: [aliceAddr, bobAddr, initialHash, bobFinalHash,
-                  finalTime, querySize, roundDuration]
-    }).send({ from: aliceAddr, gas: 1500000 })
-      .on('receipt');
+    let depthInterface = await DepthInterface
+        .new(accounts[0], accounts[1], bobMM.merkel(), roundDuration,
+             { from: accounts[2], gas: 2000000 });
 
     // check if the state is WaitingHashes
-    currentState = yield depthContract.methods
-      .currentState().call({ from: bobAddr });
-    expect(currentState).to.equal('1');
+    currentState = await depthInterface.currentState.call();
+    expect(currentState.toNumber()).to.equal(1);
 
     // mimic a waiting period of 3500 seconds
-    response = yield sendRPC({jsonrpc: "2.0", method: "evm_increaseTime",
-                              params: [3500], id: 0});
+    response = await sendRPC(web3, {jsonrpc: "2.0", method: "evm_increaseTime",
+                                    params: [3500], id: 0});
 
     // alice claiming victory should fail
-    response = yield depthContract.methods
-      .claimVictoryByTime()
-      .send({ from: aliceAddr, gas: 1500000 })
-      .catch(function(error) {
-        expect(error.message).to.have.string('VM Exception');
-      });
+    expect(await getError(
+      depthInterface.claimVictoryByTime({ from: accounts[0], gas: 1500000 }))
+          ).to.have.string('VM Exception');
 
     // mimic a waiting period of 200 seconds
-    response = yield sendRPC({jsonrpc: "2.0", method: "evm_increaseTime",
-                              params: [200], id: 0});
+    response = await sendRPC(web3, {jsonrpc: "2.0", method: "evm_increaseTime",
+                                    params: [200], id: 0});
 
     // alice claiming victory should now work
-    response = yield depthContract.methods
-      .claimVictoryByTime()
-      .send({ from: aliceAddr, gas: 1500000 });
-    returnValues = response.events.ChallengeEnded.returnValues;
-    expect(+returnValues.theState).to.equal(2);
+    response = await depthInterface
+      .claimVictoryByTime({ from: accounts[0], gas: 1500000 });
+    event = getEvent(response, 'ChallengeEnded');
+    expect(+event.theState.toNumber()).to.equal(2);
 
     // check if the state is ChallengerWon
-    currentState = yield depthContract.methods
-      .currentState().call({ from: bobAddr });
-    expect(currentState).to.equal('2');
+    currentState = await depthInterface.currentState.call();
+    expect(currentState.toNumber()).to.equal(2);
 
     // kill contract
-    response = yield depthContract.methods.kill()
-      .send({ from: aliceAddr, gas: 1500000 });
+    response = await depthInterface.kill({ from: accounts[2], gas: 1500000 });
 
+    // check if contract was killed
+    [error, currentState] = await unwrap(depthInterface.currentState());
+    expect(error.message).to.have.string('not a contract address');;
   });
 
-  it('Challenger timeout', function*() {
-    this.timeout(15000)
-
+  it('Challenger timeout', async function() {
     // deploy contract and update object
-    depthContract = yield depthContract.deploy({
-      data: bytecode,
-      arguments: [aliceAddr, bobAddr, initialHash, bobFinalHash,
-                  finalTime, querySize, roundDuration]
-    }).send({ from: aliceAddr, gas: 1500000 })
-      .on('receipt');
+    let depthInterface = await DepthInterface
+        .new(accounts[0], accounts[1], bobMM.merkel(), roundDuration,
+             { from: accounts[2], gas: 2000000 });
 
     // check if the state is WaitingHashes
-    currentState = yield depthContract.methods
-      .currentState().call({ from: bobAddr });
-    expect(currentState).to.equal('1');
+    currentState = await depthInterface.currentState.call();
+    expect(currentState.toNumber()).to.equal(1);
 
-    // create empty arrays for query and reply
-    queryArray = [];
-    replyArray = [];
-    for (i = 0; i < querySize; i++) queryArray.push(0);
-    for (i = 0; i < querySize; i++) replyArray.push("");
+    currentDepth = await depthInterface.currentDepth.call();
+    currentAddress = await depthInterface.currentAddress.call();
+    currentSize = BigNumber(2).pow(63 - currentDepth);
+    currentLeftAddress = BigNumber(currentAddress);
+    currentRightAddress = currentLeftAddress.plus(currentSize);
 
-    // get the query array and prepare response
-    // (loop since solidity cannot return dynamic array from function)
-    for (i = 0; i < querySize; i++) {
-        queryArray[i] = yield depthContract.methods
-            .queryArray(i).call({ from: bobAddr });
-        replyArray[i] = bobHistory[queryArray[i]];
-    }
+    // get the hashes of children and prepare response
+    leftHash = bobMM.subMerkel(bobMM.memoryMap, currentLeftAddress,
+                               60 - currentDepth);
+    rightHash = bobMM.subMerkel(bobMM.memoryMap, currentRightAddress,
+                                60 - currentDepth);
+
+    // alice claiming victory should fail
+    expect(await getError(
+      depthInterface.claimVictoryByTime({ from: accounts[0], gas: 1500000 }))
+          ).to.have.string('VM Exception');
 
     // send hashes
-    response = yield depthContract.methods
-        .replyQuery(queryArray, replyArray)
-        .send({ from: bobAddr, gas: 1500000 })
-        .on('receipt', function(receipt) {
-            expect(receipt.events.HashesPosted).not.to.be.undefined;
-        });
-    returnValues = response.events.HashesPosted.returnValues;
+    response = await depthInterface
+      .replyQuery(leftHash, rightHash,
+                  { from: accounts[1], gas: 1500000 })
+    event = getEvent(response, 'HashesPosted');
+    expect(event).not.to.be.undefined;
+
+    // check if the state is WaitingQuery
+    currentState = await depthInterface.currentState.call();
+    expect(currentState.toNumber()).to.equal(0);
 
     // mimic a waiting period of 3500 seconds
-    response = yield sendRPC({jsonrpc: "2.0", method: "evm_increaseTime",
-                              params: [3500], id: 0});
+    response = await sendRPC(web3, {jsonrpc: "2.0", method: "evm_increaseTime",
+                                    params: [3500], id: 0});
 
     // bob claiming victory should fail
-    response = yield depthContract.methods
-      .claimVictoryByTime()
-      .send({ from: bobAddr, gas: 1500000 })
-      .catch(function(error) {
-        expect(error.message).to.have.string('VM Exception');
-      });
+    expect(await getError(
+      depthInterface.claimVictoryByTime({ from: accounts[1], gas: 1500000 }))
+          ).to.have.string('VM Exception');
 
     // mimic a waiting period of 200 seconds
-    response = yield sendRPC({jsonrpc: "2.0", method: "evm_increaseTime",
-                              params: [200], id: 0});
+    response = await sendRPC(web3, {jsonrpc: "2.0", method: "evm_increaseTime",
+                                    params: [200], id: 0});
 
     // bob claiming victory should now work
-    response = yield depthContract.methods
-      .claimVictoryByTime()
-      .send({ from: bobAddr, gas: 1500000 });
-    returnValues = response.events.ChallengeEnded.returnValues;
-    expect(+returnValues.theState).to.equal(3);
+    response = await depthInterface
+      .claimVictoryByTime({ from: accounts[1], gas: 1500000 });
+    event = getEvent(response, 'ChallengeEnded');
+    expect(+event.theState).to.equal(3);
 
     // check if the state is ClaimerWon
-    currentState = yield depthContract.methods
-      .currentState().call({ from: bobAddr });
-    expect(currentState).to.equal('3');
+    currentState = await depthInterface.currentState.call();
+    expect(currentState.toNumber()).to.equal(3);
 
     // kill contract
-    response = yield depthContract.methods.kill()
-      .send({ from: aliceAddr, gas: 1500000 });
+    response = await depthInterface.kill({ from: accounts[2], gas: 1500000 });
+
+    // check if contract was killed
+    [error, currentState] = await unwrap(depthInterface.currentState());
+    expect(error.message).to.have.string('not a contract address');;
   });
 });
