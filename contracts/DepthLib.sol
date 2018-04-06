970e4d62c849f016f9d7bc4a47a312f25e96241f
Augusto Teixeira <augusto@cartesi.io>
2018-04-06 13:46:09 -0300

    Convert depth into library

--- contracts/DepthLib.sol
+++ contracts/DepthLib.sol
@@ -1,45 +1,35 @@
-/// @title Partition contract
+/// @title Depth library contract
 pragma solidity ^0.4.18;
 
-contract mortal {
-  address public owner;
-
-  function mortal() public {
-    owner = msg.sender;
-  }
-
-  function kill() public {
-    if (msg.sender == owner) selfdestruct(owner);
-  }
-}
-
-
-contract depth is mortal {
-  address public challenger;
-  address public claimer;
-
-  uint public timeOfLastMove;
-  uint public roundDuration;
-
-  bytes32 public claimerCurrentHash;
-  bytes32 public claimerLeftChildHash;
-  bytes32 public claimerRightChildHash;
-  uint8 public currentDepth;
-  uint64 public currentAddress;
-
-  bytes32 public controversialPhraseOfClaimer;
+library DepthLib {
 
   enum state { WaitingQuery, WaitingHashes,
                ChallengerWon, ClaimerWon,
                WaitingControversialPhrase,
                ControversialPhraseFound }
-  state public currentState;
+
+  struct DepthCtx {
+    address challenger;
+    address claimer;
+
+    uint timeOfLastMove;
+    uint roundDuration;
+
+    bytes32 claimerCurrentHash;
+    bytes32 claimerLeftChildHash;
+    bytes32 claimerRightChildHash;
+    uint8 currentDepth;
+    uint64 currentAddress;
+
+    bytes32 controversialPhraseOfClaimer;
+    state currentState;
+  }
 
   event QueryPosted(uint8 theCurrentDepth, uint64 theCurrentAddress);
   event HashesPosted(bytes32 theLeftHash, bytes32 theRightHash);
-  event ChallengeEnded(state theState);
+  event ChallengeEnded(uint8 theState);
   event ControversialPhrasePosted(uint64 addressStartingDivergence,
-                                bytes32 theControversialPhraseOfClaimer);
+                                  bytes32 theControversialPhraseOfClaimer);
 
   // Suppose two agents have distinct memory states, of course with different
   // Merkel-tree hashes. This contract helps them find the exact point where
@@ -62,35 +52,37 @@ contract depth is mortal {
   //              |
   //            ContFound
 
-  function depth(address theChallenger, address theClaimer,
-                 bytes32 theClaimerHashOfRoot,
-                 uint theRoundDuration) public
+  function init(DepthCtx storage self, address theChallenger,
+                address theClaimer, bytes32 theClaimerHashOfRoot,
+                uint theRoundDuration) public
   {
     require(theChallenger != theClaimer);
-    challenger = theChallenger;
-    claimer = theClaimer;
-    claimerCurrentHash = theClaimerHashOfRoot;
-    currentDepth = 0;
-    currentAddress = 0;
-    roundDuration = theRoundDuration;
-    timeOfLastMove = now;
-    currentState = state.WaitingHashes;
-    QueryPosted(currentDepth, currentAddress);
+    self.challenger = theChallenger;
+    self.claimer = theClaimer;
+    self.claimerCurrentHash = theClaimerHashOfRoot;
+    self.currentDepth = 0;
+    self.currentAddress = 0;
+    self.roundDuration = theRoundDuration;
+    self.timeOfLastMove = now;
+    self.currentState = state.WaitingHashes;
+    emit QueryPosted(self.currentDepth, self.currentAddress);
   }
 
   /// @notice Answer the query (only claimer can call it) by posting.
   /// two hashes that combine to the currentHash.
   /// @param leftHash the child hash to the left of the current one.
   /// @param rightHash the child hash to the right of the current one.
-  function replyQuery(bytes32 leftHash, bytes32 rightHash) public {
-    require(msg.sender == claimer);
-    require(currentState == state.WaitingHashes);
-    require(keccak256(leftHash, rightHash) == claimerCurrentHash);
-    claimerLeftChildHash = leftHash;
-    claimerRightChildHash = rightHash;
-    timeOfLastMove = now;
-    currentState = state.WaitingQuery;
-    HashesPosted(leftHash, rightHash);
+  function replyQuery(DepthCtx storage self, bytes32 leftHash,
+                      bytes32 rightHash) public
+  {
+    require(msg.sender == self.claimer);
+    require(self.currentState == state.WaitingHashes);
+    require(keccak256(leftHash, rightHash) == self.claimerCurrentHash);
+    self.claimerLeftChildHash = leftHash;
+    self.claimerRightChildHash = rightHash;
+    self.timeOfLastMove = now;
+    self.currentState = state.WaitingQuery;
+    emit HashesPosted(leftHash, rightHash);
     //HashesPosted(keccak256(leftHash, rightHash), claimerCurrentHash);
   }
 
@@ -98,25 +90,28 @@ contract depth is mortal {
   /// direction to continue the search.
   /// @param continueToTheLeft a boolean saying if we should continue to the
   /// left (otherwise we continue to the right)
-  function makeQuery(bool continueToTheLeft, bytes32 differentHash) public {
-    require(msg.sender == challenger);
-    require(currentState == state.WaitingQuery);
+  function makeQuery(DepthCtx storage self, bool continueToTheLeft,
+                     bytes32 differentHash) public
+  {
+    require(msg.sender == self.challenger);
+    require(self.currentState == state.WaitingQuery);
     if(continueToTheLeft) {
-      claimerCurrentHash = claimerLeftChildHash;
+      self.claimerCurrentHash = self.claimerLeftChildHash;
     } else {
-      claimerCurrentHash = claimerRightChildHash;
-      currentAddress = currentAddress + uint64(2)**uint64(63 - currentDepth);
+      self.claimerCurrentHash = self.claimerRightChildHash;
+      self.currentAddress = self.currentAddress
+        + uint64(2)**uint64(63 - self.currentDepth);
     }
     // test if challenger knows the new problematic hash
-    require(claimerCurrentHash == differentHash);
-    currentDepth = currentDepth + 1;
-    if (currentDepth == 59) {
-      currentState = state.WaitingControversialPhrase;
+    require(self.claimerCurrentHash == differentHash);
+    self.currentDepth = self.currentDepth + 1;
+    if (self.currentDepth == 59) {
+      self.currentState = state.WaitingControversialPhrase;
     } else {
-      currentState = state.WaitingHashes;
+      self.currentState = state.WaitingHashes;
     }
-    timeOfLastMove = now;
-    QueryPosted(currentDepth, currentAddress);
+    self.timeOfLastMove = now;
+    emit QueryPosted(self.currentDepth, self.currentAddress);
   }
 
   /// @notice Post hash that was found different between claimer and challenger
@@ -124,45 +119,54 @@ contract depth is mortal {
   /// @param word2 second word composing hash
   /// @param word3 third word composing hash
   /// @param word4 forth word composing hash
-  function postControversialPhrase(bytes8 word1, bytes8 word2,
-                                   bytes8 word3, bytes8 word4) public {
-    require(msg.sender == claimer);
-    require(currentState == state.WaitingControversialPhrase);
-    require(currentDepth == 59);
+  function postControversialPhrase(DepthCtx storage self, bytes8 word1,
+                                   bytes8 word2, bytes8 word3,
+                                   bytes8 word4) public
+  {
+    require(msg.sender == self.claimer);
+    require(self.currentState == state.WaitingControversialPhrase);
+    require(self.currentDepth == 59);
     bytes32 word1Hash = keccak256(word1);
     bytes32 word2Hash = keccak256(word2);
     bytes32 word3Hash = keccak256(word3);
     bytes32 word4Hash = keccak256(word4);
     bytes32 word12Hash = keccak256(word1Hash, word2Hash);
     bytes32 word34Hash = keccak256(word3Hash, word4Hash);
-    require(keccak256(word12Hash, word34Hash) == claimerCurrentHash);
-    controversialPhraseOfClaimer = bytes32(word1);
-    controversialPhraseOfClaimer |= bytes32(word2) >> 64;
-    controversialPhraseOfClaimer |= bytes32(word3) >> 128;
-    controversialPhraseOfClaimer |= bytes32(word4) >> 192;
-    currentState = state.ControversialPhraseFound;
-    ControversialPhrasePosted(currentAddress, controversialPhraseOfClaimer);
-    ChallengeEnded(currentState);
+    require(keccak256(word12Hash, word34Hash) == self.claimerCurrentHash);
+    self.controversialPhraseOfClaimer = bytes32(word1);
+    self.controversialPhraseOfClaimer |= bytes32(word2) >> 64;
+    self.controversialPhraseOfClaimer |= bytes32(word3) >> 128;
+    self.controversialPhraseOfClaimer |= bytes32(word4) >> 192;
+    self.currentState = state.ControversialPhraseFound;
+    emit ControversialPhrasePosted(self.currentAddress,
+                                   self.controversialPhraseOfClaimer);
+    emit ChallengeEnded(uint8(self.currentState));
   }
 
   /// @notice Claim victory for opponent timeout.
-  function claimVictoryByTime() public {
-    if ((msg.sender == claimer) && (currentState == state.WaitingQuery)
-        && (now > timeOfLastMove + roundDuration)) {
-      currentState = state.ClaimerWon;
-      ChallengeEnded(currentState);
+  function claimVictoryByTime(DepthCtx storage self) public {
+    if ((msg.sender == self.claimer)
+        && (self.currentState == state.WaitingQuery)
+        && (now > self.timeOfLastMove + self.roundDuration)) {
+      self.currentState = state.ClaimerWon;
+      emit ChallengeEnded(uint8(self.currentState));
+      return;
     }
-    if ((msg.sender == challenger) && (currentState == state.WaitingHashes)
-        && (now > timeOfLastMove + roundDuration)) {
-      currentState = state.ChallengerWon;
-      ChallengeEnded(currentState);
+    if ((msg.sender == self.challenger)
+        && (self.currentState == state.WaitingHashes)
+        && (now > self.timeOfLastMove + self.roundDuration)) {
+      self.currentState = state.ChallengerWon;
+      emit ChallengeEnded(uint8(self.currentState));
+      return;
     }
-    if ((msg.sender == challenger)
-        && (currentState == state.WaitingControversialPhrase)
-        && (now > timeOfLastMove + roundDuration)) {
-      currentState = state.ChallengerWon;
-      ChallengeEnded(currentState);
+    if ((msg.sender == self.challenger)
+        && (self.currentState == state.WaitingControversialPhrase)
+        && (now > self.timeOfLastMove + self.roundDuration)) {
+      self.currentState = state.ChallengerWon;
+      emit ChallengeEnded(uint8(self.currentState));
+      return;
     }
+    require(false);
   }
 }
 
