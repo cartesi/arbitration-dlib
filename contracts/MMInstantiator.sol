38711940380671e6d5d2f996f0aafcbc4dc4f518
Augusto Teixeira <augusto@cartesi.io>
2018-04-20 10:45:18 -0300

    Start converting partition to instantiator

--- contracts/MMInstantiator.sol
+++ contracts/MMInstantiator.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.4.18;
 
 contract MMInstantiator {
-  uint32 currentIndex = 0;
+  uint32 private currentIndex = 0;
 
   enum state { WaitingValues, Reading, Writing, UpdatingHashes,
                FinishedUpdating }
@@ -17,18 +17,15 @@ contract MMInstantiator {
     address client;
     bytes32 initialHash;
     bytes32 newHash; // hash after some write operations have been proved
-
     mapping(uint64 => bool) addressWasSubmitted; // mark address submitted
     mapping(uint64 => bytes8) valueSubmitted; // value submitted to address
-
     mapping(uint64 => bool) addressWasWritten; // marks address as written
     mapping(uint64 => bytes8) valueWritten; // value written to address
-
     uint64[] writtenAddress;
     state currentState;
   }
 
-  mapping(uint32 => MMCtx) private instances;
+  mapping(uint32 => MMCtx) private instance;
 
   event MemoryCreated(uint32 _index, bytes32 _initialHash);
   event ValueSubmitted(uint32 _index, uint64 _addressSubmitted,
@@ -47,11 +44,11 @@ contract MMInstantiator {
                        bytes32 _initialHash) public
   {
     require(_provider != _client);
-    instances[currentIndex].provider = _provider;
-    instances[currentIndex].client = _client;
-    instances[currentIndex].initialHash = _initialHash;
-    instances[currentIndex].newHash = _initialHash;
-    instances[currentIndex].currentState = state.WaitingValues;
+    instance[currentIndex].provider = _provider;
+    instance[currentIndex].client = _client;
+    instance[currentIndex].initialHash = _initialHash;
+    instance[currentIndex].newHash = _initialHash;
+    instance[currentIndex].currentState = state.WaitingValues;
     emit MemoryCreated(currentIndex, _initialHash);
     currentIndex++;
   }
@@ -63,8 +60,8 @@ contract MMInstantiator {
   function proveValue(uint32 _index, uint64 _address, bytes8 _value,
                       bytes32[] proof) public
   {
-    require(msg.sender == instances[_index].provider);
-    require(instances[_index].currentState == state.WaitingValues);
+    require(msg.sender == instance[_index].provider);
+    require(instance[_index].currentState == state.WaitingValues);
     require((_address & 7) == 0);
     require(proof.length == 61);
     bytes32 runningHash = keccak256(_value);
@@ -77,17 +74,17 @@ contract MMInstantiator {
         runningHash = keccak256(proof[i], runningHash);
       }
     }
-    require (runningHash == instances[_index].initialHash);
-    instances[_index].addressWasSubmitted[_address] = true;
-    instances[_index].valueSubmitted[_address] = _value;
+    require (runningHash == instance[_index].initialHash);
+    instance[_index].addressWasSubmitted[_address] = true;
+    instance[_index].valueSubmitted[_address] = _value;
     emit ValueSubmitted(_index, _address, _value);
   }
 
   /// @notice Stop memory insertion and start read and write phase
   function finishSubmissionPhase(uint32 _index) public {
-    require(msg.sender == instances[_index].provider);
-    require(instances[_index].currentState == state.WaitingValues);
-    instances[_index].currentState = state.Reading;
+    require(msg.sender == instance[_index].provider);
+    require(instance[_index].currentState == state.WaitingValues);
+    instance[_index].currentState = state.Reading;
     emit FinishedSubmittions(_index);
   }
 
@@ -97,11 +94,11 @@ contract MMInstantiator {
   function read(uint32 _index, uint64 _address)
     public view returns (bytes8)
   {
-    require(msg.sender == instances[_index].client);
-    require(instances[_index].currentState == state.Reading);
+    require(msg.sender == instance[_index].client);
+    require(instance[_index].currentState == state.Reading);
     require((_address & 7) == 0);
-    require(instances[_index].addressWasSubmitted[_address]);
-    return instances[_index].valueSubmitted[_address];
+    require(instance[_index].addressWasSubmitted[_address]);
+    return instance[_index].valueSubmitted[_address];
   }
 
   /// @notice writes on a slot of memory during read or write phase
@@ -111,45 +108,45 @@ contract MMInstantiator {
   function write(uint32 _index, uint64 _address, bytes8 _value)
     public
   {
-    require(msg.sender == instances[_index].client);
-    require((instances[_index].currentState == state.Reading)
-            || (instances[_index].currentState == state.Writing));
+    require(msg.sender == instance[_index].client);
+    require((instance[_index].currentState == state.Reading)
+            || (instance[_index].currentState == state.Writing));
     require((_address & 7) == 0);
-    require(instances[_index].addressWasSubmitted[_address]);
-    require(!instances[_index].addressWasWritten[_address]);
-    if (instances[_index].currentState == state.Reading) {
-      instances[_index].currentState = state.Writing;
+    require(instance[_index].addressWasSubmitted[_address]);
+    require(!instance[_index].addressWasWritten[_address]);
+    if (instance[_index].currentState == state.Reading) {
+      instance[_index].currentState = state.Writing;
       emit FinishedReading(_index);
     }
-    instances[_index].addressWasWritten[_address] = true;
-    instances[_index].valueWritten[_address] = _value;
-    instances[_index].writtenAddress.push(_address);
+    instance[_index].addressWasWritten[_address] = true;
+    instance[_index].valueWritten[_address] = _value;
+    instance[_index].writtenAddress.push(_address);
     emit ValueWritten(_index, _address, _value);
   }
 
   /// @notice Stop write (or read) phase
   function finishWritePhase(uint32 _index) public {
-    require(msg.sender == instances[_index].client);
-    require((instances[_index].currentState == state.Writing)
-            || (instances[_index].currentState == state.Reading));
-    instances[_index].currentState = state.UpdatingHashes;
+    require(msg.sender == instance[_index].client);
+    require((instance[_index].currentState == state.Writing)
+            || (instance[_index].currentState == state.Reading));
+    instance[_index].currentState = state.UpdatingHashes;
     emit FinishedWriting(_index);
   }
 
   /// @notice Update hash corresponding to write
   /// @param proof The proof that the new value is correct
   function updateHash(uint32 _index, bytes32[] proof) public {
-    require(msg.sender == instances[_index].provider);
-    require(instances[_index].currentState == state.UpdatingHashes);
-    require(instances[_index].writtenAddress.length > 0);
-    uint64 _address = instances[_index].writtenAddress
-      [instances[_index].writtenAddress.length - 1];
+    require(msg.sender == instance[_index].provider);
+    require(instance[_index].currentState == state.UpdatingHashes);
+    require(instance[_index].writtenAddress.length > 0);
+    uint64 _address = instance[_index].writtenAddress
+      [instance[_index].writtenAddress.length - 1];
     require((_address & 7) == 0);
-    require(instances[_index].addressWasSubmitted[_address]);
-    require(instances[_index].addressWasWritten[_address]);
+    require(instance[_index].addressWasSubmitted[_address]);
+    require(instance[_index].addressWasWritten[_address]);
     require(proof.length == 61);
-    bytes8 oldValue = instances[_index].valueSubmitted[_address];
-    bytes8 newValue = instances[_index].valueWritten[_address];
+    bytes8 oldValue = instance[_index].valueSubmitted[_address];
+    bytes8 newValue = instance[_index].valueWritten[_address];
     // verifying the proof of the old value
     bytes32 runningHash = keccak256(oldValue);
     uint64 eight = 8;
@@ -160,7 +157,7 @@ contract MMInstantiator {
         runningHash = keccak256(proof[i], runningHash);
       }
     }
-    require (runningHash == instances[_index].newHash);
+    require (runningHash == instance[_index].newHash);
     // find out new hash after write
     runningHash = keccak256(newValue);
     for (i = 0; i < 61; i++) {
@@ -170,75 +167,75 @@ contract MMInstantiator {
         runningHash = keccak256(proof[i], runningHash);
       }
     }
-    instances[_index].newHash = runningHash;
-    instances[_index].writtenAddress.length--;
-    emit HashUpdated(_index, _address, newValue, instances[_index].newHash);
+    instance[_index].newHash = runningHash;
+    instance[_index].writtenAddress.length--;
+    emit HashUpdated(_index, _address, newValue, instance[_index].newHash);
   }
 
   /// @notice Finishes updating the hash
   function finishUpdateHashPhase(uint32 _index) public {
-    require(msg.sender == instances[_index].provider);
-    require(instances[_index].currentState == state.UpdatingHashes);
-    require(instances[_index].writtenAddress.length == 0);
-    instances[_index].currentState = state.FinishedUpdating;
+    require(msg.sender == instance[_index].provider);
+    require(instance[_index].currentState == state.UpdatingHashes);
+    require(instance[_index].writtenAddress.length == 0);
+    instance[_index].currentState = state.FinishedUpdating;
     emit FinishedUpdating(_index);
   }
 
   // getter methods
   function provider(uint32 _index) public view returns (address) {
-    return instances[_index].provider;
+    return instance[_index].provider;
   }
 
   function client(uint32 _index) public view returns (address) {
-    return instances[_index].client;
+    return instance[_index].client;
   }
 
   function initialHash(uint32 _index) public view returns (bytes32) {
-    return instances[_index].initialHash;
+    return instance[_index].initialHash;
   }
 
   function newHash(uint32 _index) public view returns (bytes32) {
-    return instances[_index].newHash;
+    return instance[_index].newHash;
   }
 
   function currentState(uint32 _index) public view
     returns (MMInstantiator.state)
   {
-    return instances[_index].currentState;
+    return instance[_index].currentState;
   }
 
   function addressWasSubmitted(uint32 _index, uint64 key) public view
     returns (bool)
   {
-    return instances[_index].addressWasSubmitted[key];
+    return instance[_index].addressWasSubmitted[key];
   }
 
   function valueSubmitted(uint32 _index, uint64 key) public view
     returns (bytes8)
   {
-    return instances[_index].valueSubmitted[key];
+    return instance[_index].valueSubmitted[key];
   }
 
   function writtenAddress(uint32 _index, uint64 position) public view
     returns (uint64)
   {
-    return instances[_index].writtenAddress[position];
+    return instance[_index].writtenAddress[position];
   }
 
   function addressWasWritten(uint32 _index, uint64 addr) public view
     returns (bool)
   {
-    return instances[_index].addressWasWritten[addr];
+    return instance[_index].addressWasWritten[addr];
   }
 
   function valueWritten(uint32 _index, uint64 addr) public view
     returns (bytes8)
   {
-    return instances[_index].valueWritten[addr];
+    return instance[_index].valueWritten[addr];
   }
 
   function getWrittenAddressLength(uint32 _index) public view returns (uint) {
-    return instances[_index].writtenAddress.length;
+    return instance[_index].writtenAddress.length;
   }
 }
 
