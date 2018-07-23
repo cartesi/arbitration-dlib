0ffd81482a267b76734c5609680298f5ec789668
Felipe Argento <felipe.argento@goblock.com.br>
2018-07-23 15:48:19 -0300

    Renamed endOfInterval to queryLastIndex

--- contracts/PartitionInstantiator.sol
+++ contracts/PartitionInstantiator.sol
@@ -94,11 +94,11 @@ contract PartitionInstantiator is PartitionInterface, Decorated {
     require(rightPoint > leftPoint);
     uint i;
     uint intervalLength = rightPoint - leftPoint;
-    uint endOfInterval = instance[_index].querySize - 1;
+    uint queryLastIndex = instance[_index].querySize - 1;
     // if intervalLength is not big enough to allow us jump sizes larger then
     // one, we go step by step
-    if (intervalLength < 2 * endOfInterval) {
-      for (i = 0; i < endOfInterval; i++) {
+    if (intervalLength < 2 * queryLastIndex) {
+      for (i = 0; i < queryLastIndex; i++) {
         if (leftPoint + i < rightPoint) {
           instance[_index].queryArray[i] = leftPoint + i;
         } else {
@@ -109,12 +109,12 @@ contract PartitionInstantiator is PartitionInterface, Decorated {
       // otherwise: intervalLength = (querySize - 1) * divisionLength + j
       // with divisionLength >= 1 and j in {0, ..., querySize - 2}. in this
       // case the size of maximum slice drops to a proportion of intervalLength
-      uint divisionLength = intervalLength / endOfInterval;
-      for (i = 0; i < endOfInterval; i++) {
+      uint divisionLength = intervalLength / queryLastIndex;
+      for (i = 0; i < queryLastIndex; i++) {
         instance[_index].queryArray[i] = leftPoint + i * divisionLength;
       }
     }
-    instance[_index].queryArray[endOfInterval] = rightPoint;
+    instance[_index].queryArray[queryLastIndex] = rightPoint;
   }
 
   /// @notice Answer the query (only claimer can call it).
