e57422ccf475bc271d703abf7e5ed1a68d2f82da
Felipe Argento <felipe@cartesi.io>
2018-08-02 16:24:42 -0300

    Updated tests to not rely on events returning arrays

--- test/partition.js
+++ test/partition.js
@@ -244,8 +244,8 @@ contract('PartitionInstantiator', function(accounts) {
         // find first last time of query where there was aggreement
         var lastConsensualQuery = 0;
         for (i = 0; i < querySize - 1; i++){
-          if (challengerHistory[event._postedTimes[i]]
-            == event._postedHashes[i]) {
+          if (challengerHistory[queryArray[i]]
+            == replyArray[i]) {
             lastConsensualQuery = i;
           } else {
             break;
@@ -262,8 +262,10 @@ contract('PartitionInstantiator', function(accounts) {
             index,
             { from: accounts[1], gas: 1500000 }))
         ).to.have.string('VM Exception');
-        leftPoint = event._postedTimes[lastConsensualQuery];
-        rightPoint = event._postedTimes[lastConsensualQuery + 1];
+              
+        leftPoint = queryArray[lastConsensualQuery];
+        rightPoint = queryArray[lastConsensualQuery + 1];
+              
         // check if the interval is unitary
         if (+rightPoint == +leftPoint + 1) {
           // if the interval is unitary, present divergence
