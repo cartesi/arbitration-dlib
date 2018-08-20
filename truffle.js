dbb71ca3d5645412b2b0b06a4af2fa9c12b40d78
Felipe Argento <felipe.argento@goblock.com.br>
2018-08-20 17:33:52 -0300

    Increasing gas limit on truffle configuration

--- truffle.js
+++ truffle.js
@@ -1,4 +1,13 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+     development: {
+       host: "localhost",
+       port: 8545,
+       network_id: "*", // Match any network id
+       gas: 3500000
+     }
+   },
+   mocha: {
+    useColors: true
+   }
 };
