e3957c484c12d76188e3be758ad3ea05f0fa7646
Felipe Argento <felipe.argento@goblock.com.br>
2018-08-20 19:31:07 -0300

    Split TestPartitionInstantiator in two separate contracts to deal with gas limit. One of the  contracts is meant to test only throws, and the other one is meant to test examples that should work

--- truffle.js
+++ truffle.js
@@ -1,13 +1,2 @@
 module.exports = {
-  networks: {
-     development: {
-       host: "localhost",
-       port: 8545,
-       network_id: "*", // Match any network id
-       gas: 3500000
-     }
-   },
-   mocha: {
-    useColors: true
-   }
 };
