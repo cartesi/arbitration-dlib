fc9427a81a08c101e3f2f79e28e616eee7776680
Felipe Argento <felipe.argento@goblock.com.br>
2018-08-21 19:29:45 -0300

    Created tests for divergence point throws

--- test/PartitionInstantiator/TestPartitionInstantiatorThrows.sol
+++ test/PartitionInstantiator/TestPartitionInstantiatorThrows.sol
@@ -71,7 +71,6 @@ contract TestPartitionInstantiatorThrows is PartitionInstantiator{
   bool r = aliceThrowProxy.execute.gas(2000000)();
   Assert.equal(r, false, "Transaction should fail, queryPiece is bigger than instance.querySize -1");
 
-
     //Make query with incorrect leftPoint
   PartitionInstantiator(address(aliceThrowProxy)).makeQuery(newIndex, queryPiece, 0, partition.getQueryArrayAtIndex(newIndex, queryPiece + 1)); 
   
@@ -87,6 +86,36 @@ contract TestPartitionInstantiatorThrows is PartitionInstantiator{
     
   r = aliceThrowProxy.execute.gas(2000000)();
   Assert.equal(r, false, "Transaction should fail, wrong rightPoint");
+  }
+  
+  //Test present divergence throws
+  function testPresentDivergenceThrows() public {
+    uint divergenceTime = 12;
+    PartitionTestAux partition = PartitionTestAux(DeployedAddresses.PartitionTestAux());
+    ThrowProxy aliceThrowProxy = new ThrowProxy(address(partition));
+  uint newIndex = partition.instantiate(address(aliceThrowProxy),0x123,"initialHash","finalHash", 50000, 15, 55);   
+    
+    partition.setTimeSubmittedAtIndex(newIndex, divergenceTime + 1); 
+
+    //Present divergencePoint after finalTime
+    PartitionInstantiator(address(aliceThrowProxy)).presentDivergence(newIndex, 30); 
+    
+    bool r = aliceThrowProxy.execute.gas(2000000)();
+    Assert.equal(r, false, "Transaction should fail, divergence time >  final time");
+ 
+    //Present non submitted divergencePoint
+    PartitionInstantiator(address(aliceThrowProxy)).presentDivergence(newIndex, divergenceTime); 
+    
+    r = aliceThrowProxy.execute.gas(2000000)();
+    Assert.equal(r, false, "Transaction should fail, divergence point was not submitted");
+
+
+    //Present submitted divergencePoint  but divergencePoint + 1 not submitted
+    divergenceTime += 1;
+    PartitionInstantiator(address(aliceThrowProxy)).presentDivergence(newIndex, divergenceTime); 
+    
+    r = aliceThrowProxy.execute.gas(2000000)();
+    Assert.equal(r, false, "Transaction should fail, divergence point + 1 was not submitted");
   }
   // Test modifiers
   function testModifiers() public {
