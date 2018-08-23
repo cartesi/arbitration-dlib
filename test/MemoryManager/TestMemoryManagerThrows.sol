e2bd52ceb3a62db52559a5323e37b6a013f082ee
Felipe Argento <felipe.argento@goblock.com.br>
2018-08-23 16:56:50 -0300

    File structure for unit tests

---
+++ test/MemoryManager/TestMemoryManagerThrows.sol
@@ -0,0 +1,9 @@
+pragma solidity 0.4.24;
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../../contracts/MMInstantiator.sol";
+import "../../contracts/testAuxiliaries/MMInstantiatorTestAux.so";
+
+contract TestMemoryManagerThrows is MMInstantiator {
+
+}
