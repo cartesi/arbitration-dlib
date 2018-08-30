532c5a6e39b914668a116654ff9f73f4bc81c15c
Felipe Argento <felipe.argento@goblock.com.br>
2018-08-30 15:43:29 -0300

    Created MMInstantiatorTestAux to manipulate instance and directly control structures

--- contracts/testAuxiliaries/MMInstantiatorTestAux.sol
+++ contracts/testAuxiliaries/MMInstantiatorTestAux.sol
@@ -1,6 +1,8 @@
 pragma solidity 0.4.24;
- import "../MMInstantiator.sol";
- contract MMInstantiatorTestAux is MMInstantiator {
-  constructor() public {}
-
+import "../MMInstantiator.sol";
+  contract MMInstantiatorTestAux is MMInstantiator {
+    constructor() public {}
+    function setState(uint index, state toState) public {
+      instance[index].currentState = toState;
+    }
  }
