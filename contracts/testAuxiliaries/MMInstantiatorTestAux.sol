3e8e60d5dfd9557e4015a2852406441ad5f6e5a1
Felipe Argento <felipe@cartesi.io>
2018-08-31 19:26:40 -0300

    Created wrapper for function read() (throwProxy does not work with return values), crated setPointInHistory and setHistoryAtIndex to manipulate instances

--- contracts/testAuxiliaries/MMInstantiatorTestAux.sol
+++ contracts/testAuxiliaries/MMInstantiatorTestAux.sol
@@ -5,4 +5,24 @@ import "../MMInstantiator.sol";
     function setState(uint index, state toState) public {
       instance[index].currentState = toState;
     }
+    function setHistoryPointerAtIndex(uint index, uint pointer) public {
+      instance[index].historyPointer = pointer; 
+    }
+
+    function setHistoryAtIndex(uint index, bool[] listOfWasRead, uint64[] listOfPositions, bytes8[] listOfValues) public {
+      for(uint i = 0; i < listOfWasRead.length; i++) {
+        ReadWrite memory dummyReadWrite;
+        dummyReadWrite.wasRead = listOfWasRead[i];
+        dummyReadWrite.position = listOfPositions[i];
+        dummyReadWrite.value = listOfValues[i];
+
+        instance[index].history.push(dummyReadWrite);
+      }
+    }
+    
+    //Wrapper because ThrowProxy contract do not work with return values
+    //https://github.com/trufflesuite/truffle/issues/1001
+    function readWrapper(uint index, uint64 position) public {
+      read(index, position); 
+    }
  }
