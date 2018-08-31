7e63d2a2ac408fb8c8d2b61acc686261cf780376
Felipe Argento <felipe@cartesi.io>
2018-08-31 19:33:05 -0300

    Created unit tests for all read() requires and other minor changes

--- test/MemoryManager/TestMemoryManagerThrows.sol
+++ test/MemoryManager/TestMemoryManagerThrows.sol
@@ -5,9 +5,9 @@ import "../../contracts/MMInstantiator.sol";
 import "../../contracts/SimpleMemoryInstantiator.sol";
 import "../../contracts/testAuxiliaries/MMInstantiatorTestAux.sol";
 
-contract TestMemoryManagerThrows is MMInstantiator, SimpleMemoryInstantiator {
+contract TestMemoryManagerThrows is MMInstantiatorTestAux, SimpleMemoryInstantiator {
 
-  function testProveReadAndProveWrite(){
+  function testProveReadAndProveWrite() public {
     MMInstantiatorTestAux mmInstance = MMInstantiatorTestAux(DeployedAddresses.MMInstantiatorTestAux());
     ThrowProxy aliceThrowProxy = new ThrowProxy(address(mmInstance));
     bytes32[] memory bytesArray = new bytes32[](61);
@@ -20,6 +20,7 @@ contract TestMemoryManagerThrows is MMInstantiator, SimpleMemoryInstantiator {
 
     //set wrong state
     mmInstance.setState(newIndex, state.WaitingReplay);
+    
     MMInstantiator(address(aliceThrowProxy)).proveRead(newIndex, 3,"initial", bytesArray);
    
     bool r = aliceThrowProxy.execute.gas(2000000)();
@@ -43,7 +44,101 @@ contract TestMemoryManagerThrows is MMInstantiator, SimpleMemoryInstantiator {
    
     r = aliceThrowProxy.execute.gas(2000000)();
     Assert.equal(r, false, "Transaction should fail, proof != instance[index].newHash");
-  }  
+  }
+
+  function testFinishProofPhase() public {
+    MMInstantiatorTestAux mmInstance = MMInstantiatorTestAux(DeployedAddresses.MMInstantiatorTestAux());
+    ThrowProxy aliceThrowProxy = new ThrowProxy(address(mmInstance));
+   uint newIndex =  mmInstance.instantiate(address(aliceThrowProxy), 0x321,"initalHash");
+
+    //set wrong state
+    mmInstance.setState(newIndex, state.WaitingReplay);
+   
+    MMInstantiator(address(aliceThrowProxy)).finishProofPhase(newIndex);
+    
+    bool r = aliceThrowProxy.execute.gas(2000000)();
+    Assert.equal(r, false, "Transaction should fail, state should be WaitinProofs");
+
+  }
+
+  function testRead() public {
+    uint64 position = 0;
+    
+    bool[] memory listOfWasRead = new bool[](17);
+    uint64[] memory listOfPositions = new uint64[](17);
+    bytes8[] memory listOfValues = new bytes8[](17);
+
+    for(uint64 i = 0; i < listOfWasRead.length - 1; i++){
+      listOfWasRead[i] = true;
+      listOfPositions[i] = i * 8;
+      listOfValues[i] = bytes8(i);
+    }
+    //add unaligned position for testing
+    listOfWasRead[listOfWasRead.length - 1] = true;
+    listOfPositions[listOfWasRead.length - 1] = 7;
+    listOfValues[listOfWasRead.length - 1] = bytes8(3);
+    
+    MMInstantiatorTestAux mmInstance = MMInstantiatorTestAux(DeployedAddresses.MMInstantiatorTestAux());
+    ThrowProxy aliceThrowProxy = new ThrowProxy(address(mmInstance));
+    uint newIndex =  mmInstance.instantiate(0x321, address(aliceThrowProxy), "initalHash");
+
+    //set history pointer to first position
+    mmInstance.setHistoryPointerAtIndex(newIndex, 0);
+
+    //create ReadWrites and add it to the history
+    mmInstance.setHistoryAtIndex(newIndex, listOfWasRead, listOfPositions, listOfValues);
+    //readWrapper - throproxy does not work with return values
+    MMInstantiatorTestAux(address(aliceThrowProxy)).readWrapper(newIndex, position);
+    
+    bool r = aliceThrowProxy.execute.gas(2000000)();
+    Assert.equal(r, false, "Transaction should fail, state should be WaitinProofs");
+
+    //set correct state
+    mmInstance.setState(newIndex, state.WaitingReplay);
+    
+    //set unaligned position
+    position = 7;
+    //set history pointer to unaligned position
+    mmInstance.setHistoryPointerAtIndex(newIndex, listOfWasRead.length - 1);
+
+    MMInstantiatorTestAux(address(aliceThrowProxy)).readWrapper(newIndex, position);
+    
+    r = aliceThrowProxy.execute.gas(2000000)();
+    Assert.equal(r, false, "Transaction should fail, position not aligned");
+
+    //create new instance, for separate history
+    newIndex =  mmInstance.instantiate(0x321, address(aliceThrowProxy), "initalHash");
+    //set correct state
+    mmInstance.setState(newIndex, state.WaitingReplay);
+
+    //set aligned position
+    position = 8;
+    //set history pointer to second position
+    mmInstance.setHistoryPointerAtIndex(newIndex, 1);
+    
+    //add not read to the list
+    listOfWasRead[1] = false;
+
+    mmInstance.setHistoryAtIndex(newIndex, listOfWasRead, listOfPositions, listOfValues);
+
+    MMInstantiatorTestAux(address(aliceThrowProxy)).readWrapper(newIndex, position);
+    
+    r = aliceThrowProxy.execute.gas(2000000)();
+    Assert.equal(r, false, "Transaction should fail, wasRead is false");
+
+    //set aligned position
+    position = 24;
+    //set history pointer to second(should be third) position
+    mmInstance.setHistoryPointerAtIndex(newIndex, 2);
+ 
+    MMInstantiatorTestAux(address(aliceThrowProxy)).readWrapper(newIndex, position);
+    
+    r = aliceThrowProxy.execute.gas(2000000)();
+    Assert.equal(r, false, "Transaction should fail, pointInHistory.position != position");
+
+
+
+  }
 }
 
 // Proxy contract for testing throws
