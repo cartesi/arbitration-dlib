cff84b1bc255a00a9d2cb561be8831b5cf20e054
Felipe Argento <felipe.argento@goblock.com.br>
2018-09-03 18:48:03 -0300

    Test cases for finishReplayPhase

--- test/MemoryManager/TestMemoryManagerThrows.sol
+++ test/MemoryManager/TestMemoryManagerThrows.sol
@@ -60,6 +60,44 @@ contract TestMemoryManagerThrows is MMInstantiatorTestAux, SimpleMemoryInstantia
     Assert.equal(r, false, "Transaction should fail, state should be WaitinProofs");
 
   }
+
+  function testFinishReplay() {
+     MMInstantiatorTestAux mmInstance = MMInstantiatorTestAux(DeployedAddresses.MMInstantiatorTestAux());
+    ThrowProxy aliceThrowProxy = new ThrowProxy(address(mmInstance));
+   uint newIndex =  mmInstance.instantiate( 0x321,address(aliceThrowProxy),"initalHash");
+
+    //set wrong state
+    mmInstance.setState(newIndex, state.WaitingProofs);
+   
+    MMInstantiator(address(aliceThrowProxy)).finishReplayPhase(newIndex);
+    
+    bool r = aliceThrowProxy.execute.gas(2000000)();
+    Assert.equal(r, false, "Transaction should fail, state should be WaitingReplay");
+    
+    //set correct state
+    mmInstance.setState(newIndex, state.WaitingReplay);
+
+    bool[] memory listOfWasRead = new bool[](17);
+    uint64[] memory listOfPositions = new uint64[](17);
+    bytes8[] memory listOfValues = new bytes8[](17);
+
+    for(uint64 i = 0; i < listOfWasRead.length; i++){
+      listOfWasRead[i] = true;
+      listOfPositions[i] = i * 8;
+      listOfValues[i] = bytes8(i);
+    }
+    
+    //set history pointer to 0
+    mmInstance.setHistoryPointerAtIndex(newIndex, 0);
+    //create ReadWrites and add it to the history
+    mmInstance.setHistoryAtIndex(newIndex, listOfWasRead, listOfPositions, listOfValues);
+     
+    MMInstantiator(address(aliceThrowProxy)).finishReplayPhase(newIndex);
+    
+    r = aliceThrowProxy.execute.gas(2000000)();
+    Assert.equal(r, false, "Transaction should fail, historyPointer != history.length");
+    
+  }
 }
 
 // Proxy contract for testing throws
