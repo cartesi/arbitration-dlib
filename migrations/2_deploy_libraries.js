70f7204109bee108bc0baa5ce5ad64ed6082a51b
Felipe Argento <felipe@cartesi.io>
2018-10-29 17:26:36 +0800

    updating migration to include strings.sol and bitManipulation

--- migrations/2_deploy_libraries.js
+++ migrations/2_deploy_libraries.js
@@ -1,4 +1,5 @@
 var Token = artifacts.require("./lib/bokkypoobah/Token.sol")
+var Strings = artifacts.require("./lib/strings.sol")
 var MMInstantiator = artifacts.require("./MMInstantiator.sol");
 var SimpleMemoryInstantiator = artifacts.require("./SimpleMemoryInstantiator.sol");
 var Subleq = artifacts.require("./Subleq.sol");
@@ -7,17 +8,20 @@ var DepthLib = artifacts.require("./DepthLib.sol");
 var DepthInterface = artifacts.require("./DepthInterface.sol");
 var VGInstantiator = artifacts.require("./VGInstantiator.sol");
 
+var BitsManipulation = artifacts.require("./lib/BitsManipulationLibrary.sol");
 //test aux
 
 var MMInstantiatorTestAux = artifacts.require("./testAuxiliaries/MMInstantiatorTestAux.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Token);
+  deployer.deploy(Strings);
   deployer.deploy(MMInstantiator);
   deployer.deploy(SimpleMemoryInstantiator);
   deployer.deploy(Subleq);
   deployer.deploy(PartitionInstantiator);
   deployer.deploy(DepthLib);
   deployer.deploy(MMInstantiatorTestAux);
+  deployer.deploy(BitsManipulation);
   deployer.link(DepthLib, DepthInterface);
 };
