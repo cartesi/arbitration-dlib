a38f3b2da1096b8a8c3ed9a03d7c995302ee7e97
Felipe Argento <felipe@cartesi.io>
2018-10-29 17:25:29 +0800

    Added string-utils library - transforming bit string to int

---
+++ contracts/lib/BitsManipulationLibrary.sol
@@ -0,0 +1,50 @@
+/// @title Bits Manipulation Library
+
+pragma solidity 0.4.24;
+
+import "./strings.sol";
+
+//change to lib after testing
+contract BitsManipulationLibrary {
+  using strings for *;
+
+  event Print(string message);
+
+  function littleEndianToBigEndian(uint32 num) public pure returns(uint32){
+
+  }
+  function uint32ToBitString(uint32 num) public pure returns (string) {
+    bytes memory bitString = new bytes(32);
+
+    for (uint32 i = 0; i < 32; i++) {
+      bitString[31 - i] = (num % 2 == 0) ? byte("0") : byte("1");
+      num /= 2;
+    }
+    return string(bitString);
+  }
+
+  function bitStringToUint32(string bitString) public pure returns (uint32) {
+
+    var s = bitString.toSlice();
+    var delim = ".".toSlice();
+    var bitsArray = new string[](s.count(delim) + 1);
+    for(uint32 i = 0; i < bitsArray.length; i++) {
+      bitsArray[i] = s.split(delim).toString();
+    }
+
+    require(bitsArray.length <= 32);
+    uint32 num = 0;
+
+    for (i = 0; i < bitsArray.length; i++) {
+      num *= 2;
+      //cant compare string memory and literal_string
+      if (keccak256(abi.encodePacked(bitsArray[i])) == keccak256("1")) {
+        num += 1;
+      }else {
+        require(keccak256(abi.encodePacked(bitsArray[i])) == keccak256("0"));
+      }
+    }
+    return num;
+  }
+}
+
