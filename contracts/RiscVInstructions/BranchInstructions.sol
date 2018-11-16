e31d7db6b9b565cf402ebdeddcb58802cb4c2b9f
felipeargento <felipeargento@gmail.com>
2018-11-16 17:17:12 +0800

    Removing incorrect comments

--- contracts/RiscVInstructions/BranchInstructions.sol
+++ contracts/RiscVInstructions/BranchInstructions.sol
@@ -6,37 +6,31 @@ library BranchInstructions {
 
   function execute_BEQ(uint64 rs1, uint64 rs2) public returns (bool){
     emit Print("BQE");
-    //call execute_branch then:
     return rs1 == rs2;
   }
 
   function execute_BNE(uint64 rs1, uint64 rs2) public returns (bool){
     emit Print("BNE");
-    //call execute_branch then:
     return rs1 != rs2;
   }
 
   function execute_BLT(uint64 rs1, uint64 rs2) public returns (bool){
     emit Print("BLT");
-    //call execute_branch then:
     return int64(rs1) < int64(rs2);
   }
 
   function execute_BGE(uint64 rs1, uint64 rs2) public returns (bool){
     emit Print("BGE");
-    //call execute_branch then:
     return int64(rs1) >= int64(rs2);
   }
 
   function execute_BLTU(uint64 rs1, uint64 rs2) public returns (bool){
     emit Print("BLTU");
-    //call execute_branch then:
     return rs1 < rs2;
   }
 
   function execute_BGEU(uint64 rs1, uint64 rs2) public returns (bool){
     emit Print("BGEU");
-    //call execute_branch then:
     return rs1 >= rs2;
   }
 }
