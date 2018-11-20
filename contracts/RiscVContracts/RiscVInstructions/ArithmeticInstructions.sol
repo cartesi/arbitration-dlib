9a8506b37ff86d5dfef3a16dbb4a15cce3aedba2
felipeargento <felipeargento@gmail.com>
2018-11-20 15:56:08 +0800

    removing to-do comments for builtin overflows

--- contracts/RiscVContracts/RiscVInstructions/ArithmeticInstructions.sol
+++ contracts/RiscVContracts/RiscVInstructions/ArithmeticInstructions.sol
@@ -9,20 +9,17 @@ pragma solidity 0.4.24;
 // Solidity docs Twos Complement/Underflow/Overflow: 
 // https://solidity.readthedocs.io/en/latest/security-considerations.html?highlight=overflow#two-s-complement-underflows-overflows
 
-
 library ArithmeticInstructions {
   uint constant XLEN = 64;
 
   event Print(string message);
 
-  //TO-DO: _builtin_add_overflow
   function execute_ADD(uint64 rs1, uint64 rs2) returns (uint64){
     emit Print("ADD");
     //_builtin_add_overflow(rs1, rs2, &val)
     return rs1 + rs2;
   }
 
-  //TO-DO: _builtin_sub_overflow
   function execute_SUB(uint64 rs1, uint64 rs2) returns (uint64){
     emit Print("SUB");
     //_builtin_sub_overflow(rs1, rs2, &val)
@@ -76,7 +73,7 @@ library ArithmeticInstructions {
 
     return rs1 & rs2;
   }
-  
+
   function execute_MUL(uint64 rs1, uint64 rs2) returns (uint64){
     emit Print("MUL");
     int64 srs1 = int64(rs1);
@@ -85,7 +82,7 @@ library ArithmeticInstructions {
 
     return uint64(srs1 * srs2);
   }
-  
+
   //TO-DO: Use bitmanipulation library for shift
   function execute_MULH(uint64 rs1, uint64 rs2) returns (uint64){
     emit Print("MULH");
@@ -95,7 +92,7 @@ library ArithmeticInstructions {
     //SHOULD BE ARITHMETIC SHIFT - >> of signed int
     return uint64((int128(srs1) * int128(srs2)) >> 64);
   }
-  
+
   //TO-DO: Use bitmanipulation library for shift
   function execute_MULHSU(uint64 rs1, uint64 rs2) returns (uint64){
     emit Print("MULHSU");
@@ -134,7 +131,7 @@ library ArithmeticInstructions {
 
   function execute_DIVU(uint64 rs1, uint64 rs2) returns (uint64){
     emit Print("DIVU");
-    
+
     if(rs2 == 0){
       return uint64(-1);
     }else{
@@ -149,7 +146,7 @@ library ArithmeticInstructions {
     int64 srs2 = int64(rs2);
 
     if(srs2 == 0){
-      //implicit cast on C++ version - make sure this is the expectec behaviour
+      //implicit cast on C++ version - make sure this is the expected behaviour
       return uint64(srs1);
     }else if (srs1 == (int64(1 << (XLEN - 1))) && srs2 == -1){
       return 0;
@@ -157,7 +154,7 @@ library ArithmeticInstructions {
       return uint64(srs1 % srs2);
     }
   }
-  
+
   function execute_REMU(uint64 rs1, uint64 rs2) returns (uint64){
     emit Print("REMU");
 
