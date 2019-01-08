26aed98ff74499d96b403c51f7b6810655403e82
Augusto Teixeira <augusto@impa.br>
2019-01-08 16:17:57 -0200

    Improve deploy and mock

--- mock_instances.js
+++ mock_instances.js
@@ -4,11 +4,21 @@ const Web3 = require('web3');
 const fs = require('fs');
 var web3 = new Web3('http://127.0.0.1:8545');
 
-const z = "0x8888888888888888888888888888888888888888888888888888888888888888";
+const sendRPC = function(web3, param){
+  let web3Instance = web3
+  return new Promise(function(resolve, reject) {
+    web3Instance.currentProvider.send(param, function(err, data){
+      if(err !== null) return reject(err);
+      resolve(data);
+    });
+  });
+}
 
-var account = "0x45a64818b87f5310705CEc004438C7eC7Ed36368";
-var private_key =
-    "0x339565dd96968ad4fba67e320bc9cf07808298d3654634e1bcc3b46350964f6e";
+const z = "0x8888888888888888888888888888888888888888888888888888888888888888";
+const TOTAL_NUMBER = 16;
+const MAIN_ACCOUNT = "0x2ad38f50f38abc5cbcf175e1962293eecc7936de";
+const SECOND_ACCOUNT = "0x8b5432ca3423f3c310eba126c1d15809c61aa0a9";
+const THIRD_ACCOUNT = "0xc21f17badcf5b3db4fdc825f9bc281245fe20c7d";
 
 var truffle_dump =
     fs.readFileSync(
@@ -19,34 +29,57 @@ abi = JSON.parse(truffle_dump).abi;
 
 var myContract = new web3.eth.Contract(
   abi,
-  process.env.CONCERN_CONTRACT,
+  process.env.CARTESI_CONCERN_CONTRACT,
 );
 
+let claimer, challenger, duration;
+
 async function main() {
-  for (var i = 0; i < 10; i++) {
+  let current_index = parseInt(await myContract.methods.currentIndex().call());
+  console.log("current index: " + current_index);
+  for (var i = current_index; i < current_index + TOTAL_NUMBER; i++) {
+    //console.log("i = " + i
+    //            + ", current + total = " + (current_index + TOTAL_NUMBER));
+    if (i & 1) {
+      challenger = MAIN_ACCOUNT; // main account
+    } else {
+      challenger = SECOND_ACCOUNT;
+    }
+    claimer = THIRD_ACCOUNT;
+    if (i & 2) {
+      duration = "51";
+    } else {
+      duration = "10000";
+    }
     await myContract.methods.instantiate(
-      "0x45a64818b87f5310705cec004438c7ec7ed36368", // this is the correct addr
-      "0x45a64818b87f5310705cec004438c7ec7ed36363",
+      claimer,
+      challenger,
       z,
       z,
       "120",
       "10",
-      "100",
-    ).send({from: process.env.CONCERN_USER,
-            gas: "3000000"});
-    await myContract.methods.instantiate(
-      "0x45a64818b87f5310705cec004438c7ec7ed36363",
-      "0x45a64818b87f5310705cec004438c7ec7ed36361",
-      z,
-      z,
-      "120",
-      "10",
-      "100",
-    ).send({from: process.env.CONCERN_USER,
+      duration,
+    ).send({from: process.env.CARTESI_CONCERN_USER,
             gas: "3000000"});
   }
+  let new_index = await myContract.methods.currentIndex().call()
+  console.log("new index: " + new_index);
+  response = await sendRPC(web3, { jsonrpc: "2.0",
+                                   method: "evm_increaseTime",
+                                   params: [100], id: Date.now() });
+  for (var i = current_index; i < current_index + TOTAL_NUMBER; i++) {
+    if (i & 2) {
+      await myContract.methods.claimVictoryByTime(i)
+        .send({ from: THIRD_ACCOUNT, gas: 1500000 })
+        .catch((error) => {
+          console.log("Error in claim victory. i = " + i
+                      + " error = " + error);
+        });
+    }
+  }
 
-  console.log(await myContract.methods.currentIndex().call());
 }
+
+
 
 main();
