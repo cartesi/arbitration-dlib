e8cc6d549ea70c3651ff04febf66d174a8a1fb9c
Augusto Teixeira <augusto@impa.br>
2019-01-11 17:29:38 -0200

    Add Compute and change migration

--- migrations/2_deploy_libraries.js
+++ migrations/2_deploy_libraries.js
@@ -1,11 +1,24 @@
-var Token = artifacts.require("./lib/bokkypoobah/Token.sol")
+const yaml = require('js-yaml');
+const fs   = require('fs');
+
+//var Token = artifacts.require("./lib/bokkypoobah/Token.sol")
 //var Strings = artifacts.require("./lib/strings.sol")
-var MMInstantiator = artifacts.require("./MMInstantiator.sol");
-var SimpleMemoryInstantiator = artifacts.require("./SimpleMemoryInstantiator.sol");
-var Subleq = artifacts.require("./Subleq.sol");
-var PartitionInstantiator = artifacts.require("./PartitionInstantiator.sol");
-var VGInstantiator = artifacts.require("./VGInstantiator.sol");
-var TestHash = artifacts.require("./TestHash.sol");
+
+var MMPath = __dirname + "/MMInstantiator.sol";
+var SimpleMemoryPath = __dirname + "/SimpleMemoryInstantiator.sol";
+var SubleqPath = __dirname + "/Subleq.sol";
+var PartitionPath = __dirname + "/PartitionInstantiator.sol";
+var VGPath = __dirname + "/VGInstantiator.sol";
+var ComputePath = __dirname + "/ComputeInstantiator.sol";
+var TestHashPath = __dirname + "/TestHash.sol";
+
+var MMInstantiator = artifacts.require(MMPath);
+var SimpleMemoryInstantiator = artifacts.require(SimpleMemoryPath);
+var Subleq = artifacts.require(SubleqPath);
+var PartitionInstantiator = artifacts.require(PartitionPath);
+var VGInstantiator = artifacts.require(VGPath);
+var ComputeInstantiator = artifacts.require(ComputePath);
+var TestHash = artifacts.require(TestHashPath);
 
 //test aux
 
@@ -14,22 +27,68 @@ var MMInstantiatorTestAux = artifacts.require("./testAuxiliaries/MMInstantiatorT
 //test aux
 var PartitionTestAux = artifacts.require("./testAuxiliaries/PartitionTestAux.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(Token);
-//  deployer.deploy(Strings);
-  deployer.deploy(SimpleMemoryInstantiator);
-  deployer.deploy(Subleq);
-  deployer.deploy(PartitionInstantiator).then(function(){
-    return deployer.deploy(MMInstantiator).then(function() {
-      return deployer.deploy(VGInstantiator,
-                             PartitionInstantiator.address,
-                             MMInstantiator.address).then(function() {
-                               console.log("AAA " + VGInstantiator.address);
-                             });
-    })
-  })
-  deployer.deploy(PartitionTestAux);
-  deployer.deploy(MMInstantiatorTestAux);
-//  deployer.deploy(BitsManipulation);
-  deployer.deploy(TestHash);
+module.exports = function(deployer, network, accounts) {
+  deployer.then(async () => {
+    // await deployer.deploy(Token);
+    await deployer.deploy(SimpleMemoryInstantiator);
+    await deployer.deploy(Subleq);
+    let SubleqContract = await Subleq.deployed();
+    await deployer.deploy(PartitionInstantiator);
+    let PartitionContract = await PartitionInstantiator.deployed();
+    await deployer.deploy(MMInstantiator)
+    let MMContract = await MMInstantiator.deployed();
+    await deployer.deploy(VGInstantiator,
+                          PartitionContract.address,
+                          MMContract.address);
+    let VGContract = await VGInstantiator.deployed();
+    await deployer.deploy(ComputeInstantiator,
+                          VGContract.address);
+    let ComputeContract = await ComputeInstantiator.deployed();
+    await deployer.deploy(PartitionTestAux);
+    await deployer.deploy(MMInstantiatorTestAux);
+    await deployer.deploy(TestHash);
+    if (typeof process.env.CARTESI_CONFIG !== "undefined"){
+      fs.writeFile(process.env.CARTESI_CONFIG, yaml.dump({
+        url: "http://127.0.0.1:8545",
+        max_delay: 500,
+        warn_delay: 30,
+        concerns: [],
+        abis: {
+          [PartitionContract.address]: PartitionPath,
+          [MMContract.address]: MMPath,
+          [VGContract.address]: VGPath,
+          [ComputeContract.address]: ComputePath,
+        }
+      }));
+    }
+  });
 };
+
+
+
+// module.exports = function(deployer) {
+//   deployer.deploy(Token);
+// //  deployer.deploy(Strings);
+//   deployer.deploy(SimpleMemoryInstantiator);
+//   deployer.deploy(Subleq);
+//   deployer.deploy(PartitionInstantiator).then(function(){
+//     return deployer.deploy(MMInstantiator).then(function() {
+//       return deployer.deploy(
+//         VGInstantiator,
+//         PartitionInstantiator.address,
+//         MMInstantiator.address).then(function() {
+//           console.log("vg " + VGInstantiator.address);
+//           return deployer.deploy(
+//             ComputeInstantiator,
+//             VGInstantiator.address).then(function() {
+//               console.log("compute " + ComputeInstantiator.address);
+//             });
+//         });
+//     })
+//   })
+
+//   deployer.deploy(PartitionTestAux);
+//   deployer.deploy(MMInstantiatorTestAux);
+// //  deployer.deploy(BitsManipulation);
+//   deployer.deploy(TestHash);
+// };
