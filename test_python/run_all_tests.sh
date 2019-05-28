b70177b5ba97cf0efc5a93f84aa6f3c9d3a27a6c
Stephen <stephen@cartesi.io>
2019-05-29 00:19:23 +0800

    add partition instatiator throw test

--- test_python/run_all_tests.sh
+++ test_python/run_all_tests.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-python3.7 -m pytest test_partition_make_query.py test_partition_claim_victory_by_time.py test_partition_instantiator.py test_partition_getters.py test_mm_getters.py test_mm_throws.py
+all_test_files=`/bin/ls test_*.py | grep -v test_main.py`
+python3.7 -m pytest $all_test_files
