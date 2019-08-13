de2677bd90498c08eaa039812b23b183d1f1f44f
felipeargento <felipeargento@gmail.com>
2019-08-13 14:19:17 +0800

    Add license header to smart contracts

--- contracts/MMInstantiator.sol
+++ contracts/MMInstantiator.sol
@@ -1,3 +1,13 @@
+// Note: This component currently has dependencies that are licensed under the GNU GPL, version 3, and so you should treat this component as a whole as being under the GPL version 3. But all Cartesi-written code in this component is licensed under the Apache License, version 2, or a compatible permissive license, and can be used independently under the Apache v2 license. After this component is rewritten, the entire component will be released under the Apache v2 license.
+//
+// Copyright 2019 Cartesi Pte. Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
+
+
 /// @title An instantiator of memory managers
 pragma solidity ^0.5.0;
 
