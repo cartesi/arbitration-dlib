0c07ac9eff36e34e2b0b189bfd4f3bb031ba250a
Danilo Tuler <danilo@cartesi.io>
2019-11-15 15:44:30 -0500

    Deploying contracts to testnets

--- migrations/2_deploy_libraries.js
+++ migrations/2_deploy_libraries.js
@@ -1,82 +1,13 @@
-const yaml = require('js-yaml');
-const fs   = require('fs');
-var path = require('path');
+const PartitionInstantiator = artifacts.require("PartitionInstantiator");
+const MMInstantiator = artifacts.require("MMInstantiator");
+const VGInstantiator = artifacts.require("VGInstantiator");
+const ComputeInstantiator = artifacts.require("ComputeInstantiator");
 
-//var Token = artifacts.require("./lib/bokkypoobah/Token.sol")
-//var Strings = artifacts.require("./lib/strings.sol")
-
-var rel = "../build/contracts";
-
-var MMPath = path.join(__dirname, rel, "/MMInstantiator.json");
-var SimpleMemoryPath = path.join(__dirname, rel, "/SimpleMemoryInstantiator.json");
-var HasherPath = path.join(__dirname, rel, "/Hasher.json");
-var PartitionPath = path.join(__dirname, rel, "/PartitionInstantiator.json");
-var VGPath = path.join(__dirname, rel, "/VGInstantiator.json");
-var ComputePath = path.join(__dirname, rel, "/ComputeInstantiator.json");
-var TestHashPath = path.join(__dirname, rel, "/TestHash.json");
-
-var MMInstantiator = artifacts.require("./MMInstantiator.sol");
-var SimpleMemoryInstantiator = artifacts.require("./test/SimpleMemoryInstantiator.sol");
-var Hasher = artifacts.require("./test/Hasher.sol");
-var PartitionInstantiator = artifacts.require("./PartitionInstantiator.sol");
-var VGInstantiator = artifacts.require("./VGInstantiator.sol");
-var ComputeInstantiator = artifacts.require("./ComputeInstantiator.sol");
-var TestHash = artifacts.require("./test/TestHash.sol");
-
-//test aux
-
-var MMInstantiatorTestAux =
-    artifacts.require("./test/MMInstantiatorTestAux.sol");
-
-//test aux
-var PartitionTestAux =
-    artifacts.require("./test/PartitionTestAux.sol");
-
-module.exports = function(deployer, network, accounts) {
+module.exports = function(deployer) {
   deployer.then(async () => {
-    await deployer.deploy(SimpleMemoryInstantiator);
     await deployer.deploy(PartitionInstantiator);
-    let PartitionContract = await PartitionInstantiator.deployed();
-    await deployer.deploy(MMInstantiator)
-    let MMContract = await MMInstantiator.deployed();
-    await deployer.deploy(VGInstantiator,
-                          PartitionContract.address,
-                          MMContract.address);
-    await deployer.deploy(Hasher, MMContract.address);
-    let HasherContract = await Hasher.deployed();
-    let VGContract = await VGInstantiator.deployed();
-    await deployer.deploy(ComputeInstantiator,
-                          VGContract.address);
-    let ComputeContract = await ComputeInstantiator.deployed();
-    console.log("ComputeInstantiator: " + ComputeContract.address);
-    await deployer.deploy(PartitionTestAux);
-    await deployer.deploy(MMInstantiatorTestAux);
-    await deployer.deploy(TestHash);
-    if (typeof process.env.CARTESI_CONFIG_FILE_PATH !== "undefined"){
-      fs.writeFileSync(process.env.CARTESI_CONFIG_FILE_PATH, yaml.dump({
-        concerns: [
-          { contract_address: PartitionContract.address,
-            user_address: accounts[0],
-            abi: PartitionPath,
-          },
-          { contract_address: MMContract.address,
-            user_address: accounts[0],
-            abi: MMPath,
-          },
-          { contract_address: VGContract.address,
-            user_address: accounts[0],
-            abi: VGPath,
-          },
-          { contract_address: ComputeContract.address,
-            user_address: accounts[0],
-            abi: ComputePath,
-          }
-        ]
-      }));
-      fs.writeFileSync(process.env.MM_ADD_FILE_PATH,
-        MMContract.address);
-      fs.writeFileSync(process.env.COMPUTE_ADD_FILE_PATH,
-        ComputeContract.address);
-    }
+    await deployer.deploy(MMInstantiator);
+    await deployer.deploy(VGInstantiator, PartitionInstantiator.address, MMInstantiator.address);
+    await deployer.deploy(ComputeInstantiator, VGInstantiator.address);
   });
 };
--- truffle-config.js
+++ truffle-config.js
@@ -1,16 +1,40 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const project = process.env.PROJECT_ID;
+const mnemonic = process.env.MNEMONIC;
+
+const network = (name, network_id) => ({
+  provider: () => new HDWalletProvider(mnemonic, `https://${name}.infura.io/v3/${project}`),
+  network_id
+});
+
 module.exports = {
   networks: {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "*", // Match any network id
+      gas: 6283185
     },
-    coverage: {
-      host: "localhost",
-      network_id: "*",
-      port: 8555,         // <-- If you change this, also set the port option in .solcover.js.
-      gas: 0xfffffffffff, // <-- Use this high gas value
-      gasPrice: 0x01      // <-- Use this low gas price
+    ganache: {
+      host: "ganache",
+      port: 8545,
+      network_id: "*", // Match any network id
+      gas: 6283185
+    },
+    geth: {
+      host: "geth",
+      port: 8545,
+      network_id: "15", // Match any network id
+      gas: 6283185
+    },
+    ropsten: network('ropsten', 3),
+    kovan: network('kovan', 42),
+    rinkeby: network('rinkeby', 4),
+    solc: {
+     optimizer: { // Turning on compiler optimization that removes some local variables during compilation
+       enabled: true,
+       runs: 200
+      }
     }
   }
 };
