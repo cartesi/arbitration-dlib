3a1c5af76b9a07bcf52f85aeb2a4ec0a1da25375
Danilo Tuler <danilo@cartesi.io>
2019-12-12 23:45:47 -0500

    Deletes code moved to demo

--- dispatcher-entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-export CARTESI_CONCERN_KEY=`cat /opt/cartesi/etc/keys/private_key`
-/opt/cartesi/bin/arbitration_test --config_path /opt/cartesi/etc/dispatcher/config.yaml --working_path /opt/cartesi/srv/dispatcher
