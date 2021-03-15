b8288701d1172eec9987bd9dfcd170677ba0fa7c
Milton Jonathan <milton.jonathan@cartesi.io>
2021-03-15 17:23:19 -0300

    Bumping version to 0.6.0 with Avalanche support - Using solidity-util 2.0.1 and corresponding dependencies, including hardhat-deploy-ethers - Removing test deployments from default deployment tasks, but including it for local development and tests - Restoring typechain generation: no errors seem to arise now with the currently used packages (reverts changes from https://github.com/cartesi-corp/arbitration-dlib/pull/112)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,9 +3,9 @@ import { HttpNetworkUserConfig } from "hardhat/types";
 
 import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-waffle";
-// import "hardhat-typechain";
+import "hardhat-typechain";
 import "hardhat-deploy";
-
+import "hardhat-deploy-ethers";
 
 // This is a sample Buidler task. To learn how to create your own go to
 // https://buidler.dev/guides/create-task.html
@@ -29,7 +29,7 @@ const infuraNetwork = (
         url: `https://${network}.infura.io/v3/${process.env.PROJECT_ID}`,
         chainId,
         gas,
-        accounts: mnemonic ? { mnemonic } : undefined
+        accounts: mnemonic ? { mnemonic } : undefined,
     };
 };
 
@@ -38,7 +38,7 @@ const config: HardhatUserConfig = {
         hardhat: mnemonic ? { accounts: { mnemonic } } : {},
         localhost: {
             url: "http://localhost:8545",
-            accounts: mnemonic ? { mnemonic } : undefined
+            accounts: mnemonic ? { mnemonic } : undefined,
         },
         ropsten: infuraNetwork("ropsten", 3, 6283185),
         rinkeby: infuraNetwork("rinkeby", 4, 6283185),
@@ -47,33 +47,38 @@ const config: HardhatUserConfig = {
         matic_testnet: {
             url: "https://rpc-mumbai.matic.today",
             chainId: 80001,
-            accounts: mnemonic ? { mnemonic } : undefined
+            accounts: mnemonic ? { mnemonic } : undefined,
         },
         bsc_testnet: {
             url: "https://data-seed-prebsc-1-s1.binance.org:8545",
             chainId: 97,
-            accounts: mnemonic ? { mnemonic } : undefined
-        }
+            accounts: mnemonic ? { mnemonic } : undefined,
+        },
+        avax_testnet: {
+            url: "https://api.avax-test.network/ext/bc/C/rpc",
+            chainId: 0xa869,
+            accounts: mnemonic ? { mnemonic } : undefined,
+        },
     },
     solidity: {
         version: "0.7.4",
         settings: {
             optimizer: {
-                enabled: true
-            }
-        }
+                enabled: true,
+            },
+        },
     },
     paths: {
         artifacts: "artifacts",
         deploy: "deploy",
-        deployments: "deployments"
+        deployments: "deployments",
     },
     external: {
         contracts: [
             {
                 artifacts: "node_modules/@cartesi/util/export/artifacts",
                 deploy: "node_modules/@cartesi/util/dist/deploy",
-            }
+            },
         ],
         deployments: {
             localhost: ["node_modules/@cartesi/util/deployments/localhost"],
@@ -81,25 +86,30 @@ const config: HardhatUserConfig = {
             rinkeby: ["node_modules/@cartesi/util/deployments/rinkeby"],
             kovan: ["node_modules/@cartesi/util/deployments/kovan"],
             goerli: ["node_modules/@cartesi/util/deployments/goerli"],
-            matic_testnet: ["node_modules/@cartesi/util/deployments/matic_testnet"],
-            bsc_testnet: ["node_modules/@cartesi/util/deployments/bsc_testnet"]
-        }
+            matic_testnet: [
+                "node_modules/@cartesi/util/deployments/matic_testnet",
+            ],
+            bsc_testnet: ["node_modules/@cartesi/util/deployments/bsc_testnet"],
+            avax_testnet: [
+                "node_modules/@cartesi/util/deployments/avax_testnet",
+            ],
+        },
     },
-    // typechain: {
-    //     outDir: "src/types",
-    //     target: "ethers-v5"
-    // },
+    typechain: {
+        outDir: "src/types",
+        target: "ethers-v5",
+    },
     namedAccounts: {
         deployer: {
-            default: 0
+            default: 0,
         },
         alice: {
-            default: 0
+            default: 0,
         },
         proxy: {
-            default: 1
-        }
-    }
+            default: 1,
+        },
+    },
 };
 
 export default config;
